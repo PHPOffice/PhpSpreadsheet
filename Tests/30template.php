678b40bb244da58f35c1c1346142173006ddbecb
Mark Baker <mark@lange.demon.co.uk>
2010-09-29 22:26:10 +0000

    Added 30template.php test
    git-svn-id: https://phpexcel.svn.codeplex.com/svn/trunk@61678 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Tests/30template.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * PHPExcel
+ *
+ * Copyright (C) 2006 - 2010 PHPExcel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category   PHPExcel
+ * @package    PHPExcel
+ * @copyright  Copyright (c) 2006 - 2010 PHPExcel (http://www.codeplex.com/PHPExcel)
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
+ * @version    ##VERSION##, ##DATE##
+ */
+
+/** Error reporting */
+error_reporting(E_ALL);
+
+date_default_timezone_set('Europe/London');
+
+/** PHPExcel_IOFactory */
+require_once '../Classes/PHPExcel/IOFactory.php';
+
+
+
+echo date('H:i:s') . " Load from Excel5 template\n";
+$objReader = PHPExcel_IOFactory::createReader('Excel5');
+$objPHPExcel = $objReader->load("templates/30template.xls");
+
+
+
+
+echo date('H:i:s') . " Add new data to the template\n";
+$data = array(array('title'		=> 'Excel for dummies',
+					'price'		=> 17.99,
+					'quantity'	=> 2
+				   ),
+			  array('title'		=> 'PHP for dummies',
+					'price'		=> 15.99,
+					'quantity'	=> 1
+				   ),
+			  array('title'		=> 'Inside OOP',
+					'price'		=> 12.95,
+					'quantity'	=> 1
+				   )
+			 );
+
+$objPHPExcel->getActiveSheet()->setCellValue('D1', PHPExcel_Shared_Date::PHPToExcel(time()));
+
+$baseRow = 5;
+foreach($data as $r => $dataRow) {
+	$row = $baseRow + $r;
+	$objPHPExcel->getActiveSheet()->insertNewRowBefore($row,1);
+
+	$objPHPExcel->getActiveSheet()->setCellValue('A'.$row, $r+1);
+	$objPHPExcel->getActiveSheet()->setCellValue('B'.$row, $dataRow['title']);
+	$objPHPExcel->getActiveSheet()->setCellValue('C'.$row, $dataRow['price']);
+	$objPHPExcel->getActiveSheet()->setCellValue('D'.$row, $dataRow['quantity']);
+	$objPHPExcel->getActiveSheet()->setCellValue('E'.$row, '=C'.$row.'*D'.$row);
+}
+$objPHPExcel->getActiveSheet()->removeRow($baseRow-1,1);
+
+
+
+
+echo date('H:i:s') . " Write to Excel5 format\n";
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+$objWriter->save(str_replace('.php', '.xls', __FILE__));
+
+
+
+
+// Echo memory peak usage
+echo date('H:i:s') . " Peak memory usage: " . (memory_get_peak_usage(true) / 1024 / 1024) . " MB\r\n";
+
+// Echo done
+echo date('H:i:s') . " Done writing file.\r\n";
--- Tests/runall.php
+++ Tests/runall.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -59,9 +59,12 @@ $aTests = array(
 	, '27imagesexcel5.php'
 	, '28iterator.php'
 	, '29advancedvaluebinder.php'
+	, '30template.php'
 	, 'OOCalcReader.php'
 	, 'SylkReader.php'
+	, 'Excel2003XMLReader.php'
 	, 'XMLReader.php'
+	, 'GnumericReader.php'
 );
 
 // First, clear all results
