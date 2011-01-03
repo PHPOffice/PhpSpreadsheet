b321f33fedb7ed4edaaf1f734bf29d1712b85132
Mark Baker <mark@lange.demon.co.uk>
2011-01-03 22:39:51 +0000

    Bugfix:	Work item 15029 - coordinateFromString throws exception for rows greater than 99,999
    git-svn-id: https://phpexcel.svn.codeplex.com/svn/trunk@66366 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Classes/PHPExcel/Cell.php
+++ Classes/PHPExcel/Cell.php
@@ -498,7 +498,7 @@ class PHPExcel_Cell
 	 */
 	public static function coordinateFromString($pCoordinateString = 'A1')
 	{
-		if (preg_match("/^([$]?[A-Z]{1,3})([$]?\d{1,5})$/", $pCoordinateString, $matches)) {
+		if (preg_match("/^([$]?[A-Z]{1,3})([$]?\d{1,7})$/", $pCoordinateString, $matches)) {
 			return array($matches[1],$matches[2]);
 		} elseif ((strpos($pCoordinateString,':') !== false) || (strpos($pCoordinateString,',') !== false)) {
 			throw new Exception('Cell coordinate string can not be a range of cells.');
