135c0185f5209c909655cd5c188d735f31b29ed9
Mark Baker <mark@lange.demon.co.uk>
2011-11-28 14:13:08 +0000

    Use PHPExcel_Shared_File::sys_get_temp_dir() for temporary zip file directory rather than ./
    git-svn-id: https://phpexcel.svn.codeplex.com/svn/trunk@83400 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Classes/PHPExcel/Writer/Excel2007.php
+++ Classes/PHPExcel/Writer/Excel2007.php
@@ -193,7 +193,7 @@ class PHPExcel_Writer_Excel2007 implements PHPExcel_Writer_IWriter
 			// If $pFilename is php://output or php://stdout, make it a temporary file...
 			$originalFilename = $pFilename;
 			if (strtolower($pFilename) == 'php://output' || strtolower($pFilename) == 'php://stdout') {
-				$pFilename = @tempnam('./', 'phpxltmp');
+				$pFilename = @tempnam(PHPExcel_Shared_File::sys_get_temp_dir(), 'phpxltmp');
 				if ($pFilename == '') {
 					$pFilename = $originalFilename;
 				}
