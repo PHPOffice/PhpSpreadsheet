3b47bd08520b4a55e58f413c127446197dbcd7af
Mark Baker <mark@lange.demon.co.uk>
2012-02-14 23:30:00 +0000

    Rich text should be inline rather than string... still needs testing for readers/writers of all file formats
    git-svn-id: https://phpexcel.svn.codeplex.com/svn/trunk@86490 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Classes/PHPExcel/Cell/DefaultValueBinder.php
+++ Classes/PHPExcel/Cell/DefaultValueBinder.php
@@ -81,7 +81,7 @@ class PHPExcel_Cell_DefaultValueBinder implements PHPExcel_Cell_IValueBinder
 			return PHPExcel_Cell_DataType::TYPE_STRING;
 
 		} elseif ($pValue instanceof PHPExcel_RichText) {
-			return PHPExcel_Cell_DataType::TYPE_STRING;
+			return PHPExcel_Cell_DataType::TYPE_INLINE;
 
 		} elseif ($pValue{0} === '=' && strlen($pValue) > 1) {
 			return PHPExcel_Cell_DataType::TYPE_FORMULA;
