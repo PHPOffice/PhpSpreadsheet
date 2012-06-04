6dc94c6e044407ab258a51f05781333f8121fd50
Mark Baker <mark@lange.demon.co.uk>
2012-06-04 16:20:43 +0000

    Refactor individual library PDF writers to use a common core class for all shared methods
    git-svn-id: https://phpexcel.svn.codeplex.com/svn/trunk@91320 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Classes/PHPExcel/Writer/PDF/DomPDF.php
+++ Classes/PHPExcel/Writer/PDF/DomPDF.php
@@ -2,7 +2,7 @@
 /**
  * PHPExcel
  *
- * Copyright (c) 2006 - 2011 PHPExcel
+ * Copyright (c) 2006 - 2012 PHPExcel
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,7 +20,7 @@
  *
  * @category	PHPExcel
  * @package		PHPExcel_Writer
- * @copyright	Copyright (c) 2006 - 2011 PHPExcel (http://www.codeplex.com/PHPExcel)
+ * @copyright	Copyright (c) 2006 - 2012 PHPExcel (http://www.codeplex.com/PHPExcel)
  * @license		http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
  * @version		##VERSION##, ##DATE##
  */
@@ -34,113 +34,9 @@ require_once PHPExcel_Settings::getPdfRendererPath() . '/dompdf_config.inc.php';
  *
  * @category	PHPExcel
  * @package		PHPExcel_Writer
- * @copyright	Copyright (c) 2006 - 2011 PHPExcel (http://www.codeplex.com/PHPExcel)
+ * @copyright	Copyright (c) 2006 - 2012 PHPExcel (http://www.codeplex.com/PHPExcel)
  */
-class PHPExcel_Writer_PDF_DomPDF extends PHPExcel_Writer_HTML implements PHPExcel_Writer_IWriter {
-	/**
-	 * Temporary storage directory
-	 *
-	 * @var string
-	 */
-	private $_tempDir = '';
-
-	/**
-	 * Font
-	 *
-	 * @var string
-	 */
-	private $_font = 'freesans';
-
-	/**
-	 * Orientation (Over-ride)
-	 *
-	 * @var string
-	 */
-	private $_orientation	= null;
-
-	/**
-	 * Paper size (Over-ride)
-	 *
-	 * @var int
-	 */
-	private $_paperSize	= null;
-
-
-	/**
-	 * Paper Sizes xRef List
-	 *
-	 * @var array
-	 */
-	private static $_paperSizes = array(
-		//	Excel Paper Size													TCPDF Paper Size
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_LETTER							=> 'letter',				//	(8.5 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_LETTER_SMALL					=> 'letter',				//	(8.5 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_TABLOID							=> 'tabloid',				//	(11 in. by 17 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_LEDGER							=> 'ledger',				//	(17 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_LEGAL							=> 'legal',					//	(8.5 in. by 14 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_STATEMENT						=> array(396.00,612.00),	//	(5.5 in. by 8.5 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_EXECUTIVE						=> 'executive',				//	(7.25 in. by 10.5 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A3								=> 'a3',					//	(297 mm by 420 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4								=> 'a4',					//	(210 mm by 297 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4_SMALL						=> 'a4',					//	(210 mm by 297 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A5								=> 'a5',					//	(148 mm by 210 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_B4								=> 'b4',					//	(250 mm by 353 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_B5								=> 'b5',					//	(176 mm by 250 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_FOLIO							=> 'folio',					//	(8.5 in. by 13 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_QUARTO							=> array(609.45,779.53),	//	(215 mm by 275 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_STANDARD_1						=> array(720.00,1008.00),	//	(10 in. by 14 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_STANDARD_2						=> array(792.00,1224.00),	//	(11 in. by 17 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_NOTE							=> 'letter',				//	(8.5 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_NO9_ENVELOPE					=> array(279.00,639.00),	//	(3.875 in. by 8.875 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_NO10_ENVELOPE					=> array(297.00,684.00),	//	(4.125 in. by 9.5 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_NO11_ENVELOPE					=> array(324.00,747.00),	//	(4.5 in. by 10.375 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_NO12_ENVELOPE					=> array(342.00,792.00),	//	(4.75 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_NO14_ENVELOPE					=> array(360.00,828.00),	//	(5 in. by 11.5 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_C								=> array(1224.00,1584.00),	//	(17 in. by 22 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_D								=> array(1584.00,2448.00),	//	(22 in. by 34 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_E								=> array(2448.00,3168.00),	//	(34 in. by 44 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_DL_ENVELOPE						=> array(311.81,623.62),	//	(110 mm by 220 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_C5_ENVELOPE						=> 'c5',					//	(162 mm by 229 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_C3_ENVELOPE						=> 'c3',					//	(324 mm by 458 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_C4_ENVELOPE						=> 'c4',					//	(229 mm by 324 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_C6_ENVELOPE						=> 'c6',					//	(114 mm by 162 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_C65_ENVELOPE					=> array(323.15,649.13),	//	(114 mm by 229 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_B4_ENVELOPE						=> 'b4',					//	(250 mm by 353 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_B5_ENVELOPE						=> 'b5',					//	(176 mm by 250 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_B6_ENVELOPE						=> array(498.90,354.33),	//	(176 mm by 125 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_ITALY_ENVELOPE					=> array(311.81,651.97),	//	(110 mm by 230 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_MONARCH_ENVELOPE				=> array(279.00,540.00),	//	(3.875 in. by 7.5 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_6_3_4_ENVELOPE					=> array(261.00,468.00),	//	(3.625 in. by 6.5 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_US_STANDARD_FANFOLD				=> array(1071.00,792.00),	//	(14.875 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_GERMAN_STANDARD_FANFOLD			=> array(612.00,864.00),	//	(8.5 in. by 12 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_GERMAN_LEGAL_FANFOLD			=> 'folio',					//	(8.5 in. by 13 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_ISO_B4							=> 'b4',					//	(250 mm by 353 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_JAPANESE_DOUBLE_POSTCARD		=> array(566.93,419.53),	//	(200 mm by 148 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_STANDARD_PAPER_1				=> array(648.00,792.00),	//	(9 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_STANDARD_PAPER_2				=> array(720.00,792.00),	//	(10 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_STANDARD_PAPER_3				=> array(1080.00,792.00),	//	(15 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_INVITE_ENVELOPE					=> array(623.62,623.62),	//	(220 mm by 220 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_LETTER_EXTRA_PAPER				=> array(667.80,864.00),	//	(9.275 in. by 12 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_LEGAL_EXTRA_PAPER				=> array(667.80,1080.00),	//	(9.275 in. by 15 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_TABLOID_EXTRA_PAPER				=> array(841.68,1296.00),	//	(11.69 in. by 18 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4_EXTRA_PAPER					=> array(668.98,912.76),	//	(236 mm by 322 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_LETTER_TRANSVERSE_PAPER			=> array(595.80,792.00),	//	(8.275 in. by 11 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4_TRANSVERSE_PAPER				=> 'a4',					//	(210 mm by 297 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_LETTER_EXTRA_TRANSVERSE_PAPER	=> array(667.80,864.00),	//	(9.275 in. by 12 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_SUPERA_SUPERA_A4_PAPER			=> array(643.46,1009.13),	//	(227 mm by 356 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_SUPERB_SUPERB_A3_PAPER			=> array(864.57,1380.47),	//	(305 mm by 487 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_LETTER_PLUS_PAPER				=> array(612.00,913.68),	//	(8.5 in. by 12.69 in.)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4_PLUS_PAPER					=> array(595.28,935.43),	//	(210 mm by 330 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A5_TRANSVERSE_PAPER				=> 'a5',					//	(148 mm by 210 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_JIS_B5_TRANSVERSE_PAPER			=> array(515.91,728.50),	//	(182 mm by 257 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A3_EXTRA_PAPER					=> array(912.76,1261.42),	//	(322 mm by 445 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A5_EXTRA_PAPER					=> array(493.23,666.14),	//	(174 mm by 235 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_ISO_B5_EXTRA_PAPER				=> array(569.76,782.36),	//	(201 mm by 276 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A2_PAPER						=> 'a2',					//	(420 mm by 594 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A3_TRANSVERSE_PAPER				=> 'a3',					//	(297 mm by 420 mm)
-		PHPExcel_Worksheet_PageSetup::PAPERSIZE_A3_EXTRA_TRANSVERSE_PAPER		=> array(912.76,1261.42)	//	(322 mm by 445 mm)
-	);
-
+class PHPExcel_Writer_PDF_DomPDF extends PHPExcel_Writer_PDF_Core implements PHPExcel_Writer_IWriter {
 	/**
 	 * Create a new PHPExcel_Writer_PDF
 	 *
@@ -148,63 +44,7 @@ class PHPExcel_Writer_PDF_DomPDF extends PHPExcel_Writer_HTML implements PHPExce
 	 */
 	public function __construct(PHPExcel $phpExcel) {
 		parent::__construct($phpExcel);
-		$this->setUseInlineCss(true);
-		$this->_tempDir = PHPExcel_Shared_File::sys_get_temp_dir();
 	}
-
-	/**
-	 * Set font. Examples:
-	 *   'arialunicid0-chinese-simplified'
-	 *   'arialunicid0-chinese-traditional'
-	 *   'arialunicid0-korean'
-	 *   'arialunicid0-japanese'
-	 *
-	 * @param	string	$fontName
-	 */
-	public function setFont($fontName) {
-		$this->_font = $fontName;
-		return $this;
-	}
-
-    /**
-     * Get Paper Size
-     *
-     * @return int
-     */
-    public function getPaperSize() {
-    	return $this->_paperSize;
-    }
-
-    /**
-     * Set Paper Size
-     *
-     * @param int $pValue
-     * @return PHPExcel_Writer_PDF
-     */
-    public function setPaperSize($pValue = PHPExcel_Worksheet_PageSetup::PAPERSIZE_LETTER) {
-    	$this->_paperSize = $pValue;
-    	return $this;
-    }
-
-    /**
-     * Get Orientation
-     *
-     * @return string
-     */
-    public function getOrientation() {
-    	return $this->_orientation;
-    }
-
-    /**
-     * Set Orientation
-     *
-     * @param string $pValue
-     * @return PHPExcel_Writer_PDF
-     */
-    public function setOrientation($pValue = PHPExcel_Worksheet_PageSetup::ORIENTATION_DEFAULT) {
-    	$this->_orientation = $pValue;
-    	return $this;
-    }
 
 	/**
 	 * Save PHPExcel to file
@@ -253,13 +93,13 @@ class PHPExcel_Writer_PDF_DomPDF extends PHPExcel_Writer_HTML implements PHPExce
 		}
 
 		//	Override Page Orientation
-		if (!is_null($this->_orientation)) {
-			$orientation = ($this->_orientation == PHPExcel_Worksheet_PageSetup::ORIENTATION_DEFAULT) ?
-				PHPExcel_Worksheet_PageSetup::ORIENTATION_PORTRAIT : $this->_orientation;
+		if (!is_null($this->getOrientation())) {
+			$orientation = ($this->getOrientation() == PHPExcel_Worksheet_PageSetup::ORIENTATION_DEFAULT) ?
+				PHPExcel_Worksheet_PageSetup::ORIENTATION_PORTRAIT : $this->getOrientation();
 		}
 		//	Override Paper Size
-		if (!is_null($this->_paperSize)) {
-			$printPaperSize = $this->_paperSize;
+		if (!is_null($this->getPaperSize())) {
+			$printPaperSize = $this->getPaperSize();
 		}
 
 
@@ -293,28 +133,4 @@ class PHPExcel_Writer_PDF_DomPDF extends PHPExcel_Writer_HTML implements PHPExce
 		PHPExcel_Calculation::setArrayReturnType($saveArrayReturnType);
 	}
 
-	/**
-	 * Get temporary storage directory
-	 *
-	 * @return string
-	 */
-	public function getTempDir() {
-		return $this->_tempDir;
-	}
-
-	/**
-	 * Set temporary storage directory
-	 *
-	 * @param 	string	$pValue		Temporary storage directory
-	 * @throws 	Exception	Exception when directory does not exist
-	 * @return PHPExcel_Writer_PDF
-	 */
-	public function setTempDir($pValue = '') {
-		if (is_dir($pValue)) {
-			$this->_tempDir = $pValue;
-		} else {
-			throw new Exception("Directory does not exist: $pValue");
-		}
-		return $this;
-	}
 }
