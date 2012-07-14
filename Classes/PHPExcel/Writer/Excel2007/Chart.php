e2a2fdb33588b6570d58702402fe76b1408cbba7
Mark Baker <mark@lange.demon.co.uk>
2012-07-14 01:26:53 +0100

    Bugfix: (seltzlab) Fix to excel2007 Chart Writer when a $plotSeriesValues is empty

--- Classes/PHPExcel/Writer/Excel2007/Chart.php
+++ Classes/PHPExcel/Writer/Excel2007/Chart.php
@@ -953,14 +953,17 @@ class PHPExcel_Writer_Excel2007_Chart extends PHPExcel_Writer_Excel2007_WriterPa
 						$objWriter->writeAttribute('val', $plotSeriesValues->getPointCount() );
 					$objWriter->endElement();
 
-					foreach($plotSeriesValues->getDataValues() as $plotSeriesKey => $plotSeriesValue) {
-						$objWriter->startElement('c:pt');
-							$objWriter->writeAttribute('idx', $plotSeriesKey );
+					$dataValues = $plotSeriesValues->getDataValues();
+					if (!empty($dataValues)) {
+						foreach($dataValues as $plotSeriesKey => $plotSeriesValue) {
+							$objWriter->startElement('c:pt');
+								$objWriter->writeAttribute('idx', $plotSeriesKey );
 
-							$objWriter->startElement('c:v');
-								$objWriter->writeRawData( $plotSeriesValue );
+								$objWriter->startElement('c:v');
+									$objWriter->writeRawData( $plotSeriesValue );
+								$objWriter->endElement();
 							$objWriter->endElement();
-						$objWriter->endElement();
+						}
 					}
 
 				$objWriter->endElement();
