b7dfeb9bb7b3e4328fe0c1fcf43490a3e2c2113f
Mark Baker <mark@lange.demon.co.uk>
2012-07-24 13:21:31 +0100

    Unit Tests for Date helper functions

--- unitTests/PHPExcel/Shared/DateTest.php
+++ unitTests/PHPExcel/Shared/DateTest.php
@@ -46,6 +46,9 @@ class DateTest extends PHPUnit_Framework_TestCase
 
 		$args = func_get_args();
 		$expectedResult = array_pop($args);
+		if ($args[0] < 1) {
+			$expectedResult += gmmktime(0,0,0);
+		}
 		$result = call_user_func_array(array('PHPExcel_Shared_Date','ExcelToPHP'),$args);
 		$this->assertEquals($expectedResult, $result);
 	}
@@ -53,6 +56,48 @@ class DateTest extends PHPUnit_Framework_TestCase
     public function providerDateTimeExcelToPHP1900()
     {
     	return new testDataFileIterator('rawTestData/Shared/DateTimeExcelToPHP1900.data');
+	}
+
+    /**
+     * @dataProvider providerDateTimePHPToExcel1900
+     */
+	public function testDateTimePHPToExcel1900()
+	{
+		$result = call_user_func(
+			array('PHPExcel_Shared_Date','setExcelCalendar'),
+			PHPExcel_Shared_Date::CALENDAR_WINDOWS_1900
+		);
+
+		$args = func_get_args();
+		$expectedResult = array_pop($args);
+		$result = call_user_func_array(array('PHPExcel_Shared_Date','PHPToExcel'),$args);
+		$this->assertEquals($expectedResult, $result, NULL, 1E-5);
+	}
+
+    public function providerDateTimePHPToExcel1900()
+    {
+    	return new testDataFileIterator('rawTestData/Shared/DateTimePHPToExcel1900.data');
+	}
+
+    /**
+     * @dataProvider providerDateTimeFormattedPHPToExcel1900
+     */
+	public function testDateTimeFormattedPHPToExcel1900()
+	{
+		$result = call_user_func(
+			array('PHPExcel_Shared_Date','setExcelCalendar'),
+			PHPExcel_Shared_Date::CALENDAR_WINDOWS_1900
+		);
+
+		$args = func_get_args();
+		$expectedResult = array_pop($args);
+		$result = call_user_func_array(array('PHPExcel_Shared_Date','FormattedPHPToExcel'),$args);
+		$this->assertEquals($expectedResult, $result, NULL, 1E-5);
+	}
+
+    public function providerDateTimeFormattedPHPToExcel1900()
+    {
+    	return new testDataFileIterator('rawTestData/Shared/DateTimeFormattedPHPToExcel1900.data');
 	}
 
     /**
@@ -67,6 +112,9 @@ class DateTest extends PHPUnit_Framework_TestCase
 
 		$args = func_get_args();
 		$expectedResult = array_pop($args);
+		if ($args[0] < 1) {
+			$expectedResult += gmmktime(0,0,0);
+		}
 		$result = call_user_func_array(array('PHPExcel_Shared_Date','ExcelToPHP'),$args);
 		$this->assertEquals($expectedResult, $result);
 	}
@@ -74,6 +122,27 @@ class DateTest extends PHPUnit_Framework_TestCase
     public function providerDateTimeExcelToPHP1904()
     {
     	return new testDataFileIterator('rawTestData/Shared/DateTimeExcelToPHP1904.data');
+	}
+
+    /**
+     * @dataProvider providerDateTimePHPToExcel1904
+     */
+	public function testDateTimePHPToExcel1904()
+	{
+		$result = call_user_func(
+			array('PHPExcel_Shared_Date','setExcelCalendar'),
+			PHPExcel_Shared_Date::CALENDAR_MAC_1904
+		);
+
+		$args = func_get_args();
+		$expectedResult = array_pop($args);
+		$result = call_user_func_array(array('PHPExcel_Shared_Date','PHPToExcel'),$args);
+		$this->assertEquals($expectedResult, $result, NULL, 1E-5);
+	}
+
+    public function providerDateTimePHPToExcel1904()
+    {
+    	return new testDataFileIterator('rawTestData/Shared/DateTimePHPToExcel1904.data');
 	}
 
     /**
