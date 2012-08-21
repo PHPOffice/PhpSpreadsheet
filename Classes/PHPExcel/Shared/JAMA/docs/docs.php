8c688358ba555ae4e6a766cef43d50b81a1396a3
Mark Baker <mark@lange.demon.co.uk>
2012-08-21 23:15:17 +0100

    Elimination of a potential XSS vulnerability in JAMA documentation JAMA docs are no longer up-to-date anyway

--- Classes/PHPExcel/Shared/JAMA/docs/docs.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "includes/header.php";
-require_once "includes/navbar.php";
-require_once "sections/Home.php";
-require_once "includes/footer.php";	
-?>
--- Classes/PHPExcel/Shared/JAMA/docs/includes/navbar.php
+++
@@ -1,5 +0,0 @@
-<center>
-  <hr />
-    [ <a href="index.php">index.php</a> ] [ <a href="docs.php">docs.php</a> ] [ <a href="package.php">package.php</a> ] [ <a href="test.php">test.php</a> ] [ <a href="example.php">example.php</a> ] [ <a href="download.php">download.php</a> ]
-  <hr />
-</center>
