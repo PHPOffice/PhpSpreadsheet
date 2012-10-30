6c9d5a5c2156e1ce65fc47e35ecfc95639c94518
Progi1984 <progi1984@gmail.com>
2012-10-30 08:41:03 +0100

    #62 : Clean old files

--- Build/old/build.bat
+++
@@ -1,4 +0,0 @@
-@set PHPINSTALLDIR=C:\php\5.2.9\;C:\php5;C:\LAMP\php;C:\LAMP\php5;D:\LAMP\php5
-@set PATH=%PHPINSTALLDIR%;%PATH%;
-cls
-php build.php
--- setpath.bat
+++
@@ -1,5 +0,0 @@
-@set PHPINSTALLDIR=C:\PHP\5.2.9
-@set PATH=%PHPINSTALLDIR%;%PATH%;
-
-cd Tests
-cls
