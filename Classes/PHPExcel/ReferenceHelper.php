ab01a6c6dc85842953d335821f66d3ce6525e353
Mark Baker <mbaker@inviqa.com>
2013-04-20 23:53:25 +0100

    Additional work on bugfix GH-161: Hyperlinks break when removing rows

--- Classes/PHPExcel/ReferenceHelper.php
+++ Classes/PHPExcel/ReferenceHelper.php
@@ -68,6 +68,26 @@ class PHPExcel_ReferenceHelper
 	protected function __construct() {
 	}
 
+	private function cellSort($a, $b) {
+		list($ac,$ar) = sscanf($a,'%[A-Z]%d');
+		list($bc,$br) = sscanf($b,'%[A-Z]%d');
+
+		if ($ar == $br) {
+			return strcasecmp(sprintf('%03s',$ac), sprintf('%03s',$bc));
+		}
+		return ($ar < $br) ? -1 : 1;
+	}
+
+	private function cellReverseSort($a, $b) {
+		list($ac,$ar) = sscanf($a,'%[A-Z]%d');
+		list($bc,$br) = sscanf($b,'%[A-Z]%d');
+
+		if ($ar == $br) {
+			return 1 - strcasecmp(sprintf('%03s',$ac), sprintf('%03s',$bc));
+		}
+		return ($ar < $br) ? 1 : -1;
+	}
+
 	/**
 	 * Insert a new column, updating all possible related data
 	 *
@@ -274,9 +294,10 @@ class PHPExcel_ReferenceHelper
 		$pSheet->setComments($aNewComments); // replace the comments array
 
 		// Update worksheet: hyperlinks
-		$aHyperlinkCollection = $pNumCols > 0 || $pNumRows > 0 ? 
-			array_reverse($pSheet->getHyperlinkCollection(), true) : 
-			$pSheet->getHyperlinkCollection(); 
+		$aHyperlinkCollection = $pSheet->getHyperlinkCollection();
+		($pNumCols > 0 || $pNumRows > 0) ? 
+			uksort($aHyperlinkCollection, array('PHPExcel_ReferenceHelper','cellReverseSort')) : 
+			uksort($aHyperlinkCollection, array('PHPExcel_ReferenceHelper','cellSort')); 
 		foreach ($aHyperlinkCollection as $key => $value) {
 			$newReference = $this->updateCellReference($key, $pBefore, $pNumCols, $pNumRows);
 			if ($key != $newReference) {
