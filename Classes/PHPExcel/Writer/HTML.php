9a36ddb1a2f7a38a6d17bdf4aef5634eadd84b1d
Mark Baker <mbaker@inviqa.com>
2013-06-10 23:20:58 +0100

    Quick bugfix to PDF merge cell styling

--- Classes/PHPExcel/Writer/HTML.php
+++ Classes/PHPExcel/Writer/HTML.php
@@ -1233,7 +1233,9 @@ class PHPExcel_Writer_HTML extends PHPExcel_Writer_Abstract implements PHPExcel_
 					//	Also apply style from last cell in merge to fix borders -
 					//		relies on !important for non-none border declarations in _createCSSStyleBorder
 					$endCellCoord = PHPExcel_Cell::stringFromColumnIndex($colNum + $colSpan - 1) . ($pRow + $rowSpan);
-					$cssClass .= ' style' . $pSheet->getCell($endCellCoord)->getXfIndex();
+					if (!$this->_useInlineCss) {
+						$cssClass .= ' style' . $pSheet->getCell($endCellCoord)->getXfIndex();
+					}
 				}
 
 				// Write
