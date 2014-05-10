6d72423579b1af098bfa47351c87133676b1aeea
januar <januar@januar-K46CM.(none)>
2014-05-10 18:24:51 +0700

    change sequential validation for orientation dompdf

--- Classes/PHPExcel/Writer/PDF/DomPDF.php
+++ Classes/PHPExcel/Writer/PDF/DomPDF.php
@@ -83,6 +83,9 @@ class PHPExcel_Writer_PDF_DomPDF extends PHPExcel_Writer_PDF_Core implements PHP
             $printMargins = $this->_phpExcel->getSheet($this->getSheetIndex())->getPageMargins();
         }
 
+        
+        $orientation = ($orientation == 'L') ? 'landscape' : 'portrait';
+
         //  Override Page Orientation
         if (!is_null($this->getOrientation())) {
             $orientation = ($this->getOrientation() == PHPExcel_Worksheet_PageSetup::ORIENTATION_DEFAULT)
@@ -98,7 +101,6 @@ class PHPExcel_Writer_PDF_DomPDF extends PHPExcel_Writer_PDF_Core implements PHP
             $paperSize = self::$_paperSizes[$printPaperSize];
         }
 
-        $orientation = ($orientation == 'L') ? 'landscape' : 'portrait';
 
         //  Create PDF
         $pdf = new DOMPDF();
