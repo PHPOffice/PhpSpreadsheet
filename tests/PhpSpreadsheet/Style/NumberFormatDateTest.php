b9ce2b901e00a835acb7b9e9eb59b317dca75e35
Adrien Crivelli <adrien.crivelli@gmail.com>
2016-08-25 13:47:27 +0900

    Format date unit tests is now working

--- tests/PhpSpreadsheet/Style/NumberFormatDateTest.php
+++ tests/PhpSpreadsheet/Style/NumberFormatDateTest.php
@@ -18,8 +18,6 @@ class NumberFormatDateTest extends \PHPUnit_Framework_TestCase
      */
     public function testFormatValueWithMask()
     {
-        $this->markTestIncomplete('TODO: This test should be fixed');
-
         $args = func_get_args();
         $expectedResult = array_pop($args);
         $result = call_user_func_array([NumberFormat::class, 'toFormattedString'], $args);
