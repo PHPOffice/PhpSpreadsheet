ec9e548153b4e92abbea9a7b410496a075ee5f41
Phil Taylor <phil@phil-taylor.com>
2016-10-22 20:02:47 +0100

    Remove the term "hacked by" from the source code
    Remove the term "hacked by" from the source code to prevent it
    from being flagged by malware scanners and audit tools (such as
    the very popular myJoomla.com for Joomla sites)

--- src/PhpSpreadsheet/Shared/OLERead.php
+++ src/PhpSpreadsheet/Shared/OLERead.php
@@ -309,7 +309,7 @@ class OLERead
 
         // FIX: represent numbers correctly on 64-bit system
         // http://sourceforge.net/tracker/index.php?func=detail&aid=1487372&group_id=99160&atid=623334
-        // Hacked by Andreas Rehm 2006 to ensure correct result of the <<24 block on 32 and 64bit systems
+        // Changed by Andreas Rehm 2006 to ensure correct result of the <<24 block on 32 and 64bit systems
         $_or_24 = ord($data[$pos + 3]);
         if ($_or_24 >= 128) {
             // negative number
