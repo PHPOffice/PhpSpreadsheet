7b90bb9394fea8682c4b1d710b3804cd9046ed81
Adrien Crivelli <adrien.crivelli@gmail.com>
2017-02-16 11:53:52 +0900

    Drop checks for mbstring existence
    Since mbstring is now a hard dependency enforced by composer, it is
    no longer necessary to check for its presence and implement fallbacks.

--- src/PhpSpreadsheet/Calculation/Functions.php
+++ src/PhpSpreadsheet/Calculation/Functions.php
@@ -672,37 +672,3 @@ class Functions
         return $value;
     }
 }
-
-//
-//    Strangely, PHP doesn't have a mb_str_replace multibyte function
-//    As we'll only ever use this function with UTF-8 characters, we can simply "hard-code" the character set
-//
-if ((!function_exists('mb_str_replace')) &&
-    (function_exists('mb_substr')) && (function_exists('mb_strlen')) && (function_exists('mb_strpos'))
-) {
-    function mb_str_replace($search, $replace, $subject)
-    {
-        if (is_array($subject)) {
-            $ret = [];
-            foreach ($subject as $key => $val) {
-                $ret[$key] = mb_str_replace($search, $replace, $val);
-            }
-
-            return $ret;
-        }
-
-        foreach ((array) $search as $key => $s) {
-            if ($s == '' && $s !== 0) {
-                continue;
-            }
-            $r = !is_array($replace) ? $replace : (isset($replace[$key]) ? $replace[$key] : '');
-            $pos = mb_strpos($subject, $s, 0, 'UTF-8');
-            while ($pos !== false) {
-                $subject = mb_substr($subject, 0, $pos, 'UTF-8') . $r . mb_substr($subject, $pos + mb_strlen($s, 'UTF-8'), null, 'UTF-8');
-                $pos = mb_strpos($subject, $s, $pos + mb_strlen($r, 'UTF-8'), 'UTF-8');
-            }
-        }
-
-        return $subject;
-    }
-}
