6634ad608cf8cc768c1e640ec5e242a4af3e8b50
Maxim Bulygin <maxim.bulygin@fcg.aero>
2017-10-03 17:57:45 +0300

    INDEX(): fix getting row and col numbers from references

--- src/PhpSpreadsheet/Calculation/LookupRef.php
+++ src/PhpSpreadsheet/Calculation/LookupRef.php
@@ -623,7 +623,10 @@ class LookupRef
      */
     public static function INDEX($arrayValues, $rowNum = 0, $columnNum = 0)
     {
-        if (($rowNum < 0) || ($columnNum < 0)) {
+        $rowNum = Functions::flattenSingleValue($rowNum);
+        $columnNum = Functions::flattenSingleValue($columnNum);
+
+		if (($rowNum < 0) || ($columnNum < 0)) {
             return Functions::VALUE();
         }
 
