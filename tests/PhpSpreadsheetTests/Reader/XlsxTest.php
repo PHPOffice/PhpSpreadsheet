32a55a3f137be9e07aad369232261796d691e279
Adrien Crivelli <adrien.crivelli@gmail.com>
2017-12-17 16:34:40 +0900

    Introduce identical functional tests across several formats

--- tests/PhpSpreadsheetTests/Reader/XlsxTest.php
+++ tests/PhpSpreadsheetTests/Reader/XlsxTest.php
@@ -2,10 +2,7 @@
 
 namespace PhpOffice\PhpSpreadsheetTests\Reader;
 
-use PhpOffice\PhpSpreadsheet\Reader\Xlsx as ReaderXlsx;
-use PhpOffice\PhpSpreadsheet\Shared\File;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx as WriterXlsx;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 use PHPUnit\Framework\TestCase;
 
 class XlsxTest extends TestCase
@@ -16,32 +13,7 @@ class XlsxTest extends TestCase
     public function testLoadXlsxWithoutCellReference()
     {
         $filename = './data/Reader/XLSX/without_cell_reference.xlsx';
-        $reader = new ReaderXlsx();
+        $reader = new Xlsx();
         $reader->load($filename);
-    }
-
-    public function testFreezePane()
-    {
-        $filename = tempnam(File::sysGetTempDir(), 'phpspreadsheet');
-
-        $cellSplit = 'B2';
-        $topLeftCell = 'E5';
-
-        $spreadsheet = new Spreadsheet();
-        $active = $spreadsheet->getActiveSheet();
-        $active->freezePane($cellSplit, $topLeftCell);
-
-        $writer = new WriterXlsx($spreadsheet);
-        $writer->save($filename);
-
-        // Read written file
-        $reader = new ReaderXlsx();
-        $reloadedSpreadsheet = $reader->load($filename);
-        $reloadedActive = $reloadedSpreadsheet->getActiveSheet();
-        $actualCellSplit = $reloadedActive->getFreezePane();
-        $actualTopLeftCell = $reloadedActive->getTopLeftCell();
-
-        self::assertSame($cellSplit, $actualCellSplit, 'should be able to set freeze pane');
-        self::assertSame($topLeftCell, $actualTopLeftCell, 'should be able to set the top left cell');
     }
 }
