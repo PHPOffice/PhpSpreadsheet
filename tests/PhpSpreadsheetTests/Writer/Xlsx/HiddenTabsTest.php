a5c2a024882def55214ed31dcb0dd5a144223e34
Bill Blume <bill.blume@mobileforcesoftware.com>
2018-05-31 15:25:27 -0700

    Changes to fix #523 based on comments.
    Made the following changes to fix PHPOffice/PhpSpreadsheet#523
    based on code-review comments.
    * Updating workbook attributes is now done via a loop.
    * Unit test no longer requires an input xlsx document.

--- tests/PhpSpreadsheetTests/Writer/Xlsx/HiddenTabsTest.php
+++ tests/PhpSpreadsheetTests/Writer/Xlsx/HiddenTabsTest.php
@@ -3,30 +3,64 @@
 namespace PhpOffice\PhpSpreadsheetTests\Writer\Xlsx;
 
 use PhpOffice\PhpSpreadsheet\Settings;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PHPUnit\Framework\TestCase;
 
 class HiddenTabsTest extends TestCase
 {
+    /**
+     * Copy of PhpOffice\PhpSpreadsheet\Writer\Xlsx\Workbook::$bookViewAttributes
+     * except that the values are purposefully set to values different from the
+     * default values.
+     *
+     * @var array
+     */
+    private static $bookViewAttributes = [
+        'autoFilterDateGrouping' => '0',
+        'firstSheet' => '1',
+        'minimized' => '1',
+        'showHorizontalScroll' => '0',
+        'showSheetTabs' => '0',
+        'showVerticalScroll' => '0',
+        'tabRatio' => '601',
+        'visibility' => 'hidden',
+    ];
+
     /**
      * Test that the worksheet tabs remain hidden when reading and writing a XLSX document
      * with hidden worksheets tabs.
      */
     public function testUpdateWithHiddenTabs()
     {
-        $sourceFilename = __DIR__ . '/../../../data/Writer/XLSX/hidden_tabs.xlsx';
-        Settings::setLibXmlLoaderOptions(null); // reset to default options
-        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-        $excel = $reader->load($sourceFilename);
+        // Create a dummy workbook with two worksheets
+        $workbook = new Spreadsheet();
+        $worksheet1 = $workbook->getActiveSheet();
+        $worksheet1->setTitle('Tweedledee');
+        $worksheet1->setCellValue('A1', 1);
+        $worksheet2 = $workbook->createSheet();
+        $worksheet2->setTitle('Tweeldedum');
+        $worksheet2->setCellValue('A1', 2);
 
-        $targetFilename = tempnam(sys_get_temp_dir(), 'tst');
-        $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($excel);
+        // Set the workbook bookbiews to non-default values
+        foreach (self::$bookViewAttributes as $attr => $value) {
+            $workbook->setWorkbookViewAttribute($attr, $value);
+        }
+
+        Settings::setLibXmlLoaderOptions(null); // reset to default options
+
+        $targetFilename = tempnam(sys_get_temp_dir(), 'xlsx');
+        $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($workbook);
         $writer->save($targetFilename);
 
         try {
-            $excel2 = $reader->load($targetFilename);
-            $this->assertEquals('0', $excel2->getWorkbookViewAttribute('showSheetTabs'));
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+            $workbook2 = $reader->load($targetFilename);
+
+            foreach (self::$bookViewAttributes as $attr => $value) {
+                $this->assertEquals($value, $workbook2->getWorkbookViewAttribute($attr));
+            }
         } finally {
-            @unlink($targetFilename);
+            unlink($targetFilename);
         }
     }
 }
