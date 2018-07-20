c358ba98a130e26c862c862647a97d551fd84a0f
Christian Forgács <cf@wunderbit.de>
2018-07-20 17:09:34 +0200

    Add sum data test

--- tests/PhpSpreadsheetTests/Reader/XlsTest.php
+++ tests/PhpSpreadsheetTests/Reader/XlsTest.php
@@ -16,4 +16,63 @@ class XlsTest extends TestCase
         $reader = new Xls();
         $reader->load($filename);
     }
+
+    /**
+     * Test sum data in xls files.
+     */
+    public function testSumData()
+    {
+        $filename = './data/Reader/Xls/sum_data.xls';
+        $reader = new Xls();
+        $spreadSheet = $reader->load($filename);
+
+        $tests = [
+            'Test' => [
+                'A1' => 1,
+                'A2' => 2,
+                'A3' => 3,
+                'A4' => 4,
+                'A5' => 5,
+                'A6' => 6,
+                'A7' => 7,
+                'A8' => 8,
+                'A9' => 9,
+                'A10' => 10,
+
+                'B1' => 3,
+                'B2' => 4,
+                'B3' => 5,
+                'B4' => 6,
+                'B5' => 7,
+                'B6' => 8,
+                'B7' => 9,
+                'B8' => 10,
+                'B9' => 11,
+                'B10' => 12,
+
+                'C1' => 4,
+                'C2' => 6,
+                'C3' => 8,
+                'C4' => 10,
+                'C5' => 12,
+                'C6' => 14,
+                'C7' => 16,
+                'C8' => 18,
+                'C9' => 20,
+                'C10' => 22,
+            ]
+        ];
+
+        foreach ($tests as $sheetName => $testsForSheet) {
+            $sheet = $spreadSheet->getSheetByName($sheetName);
+
+            foreach ($testsForSheet as $cellCoordinate => $result) {
+                $calculatedValue = round($sheet->getCell($cellCoordinate)->getCalculatedValue());
+
+                if ($calculatedValue != $result) {
+                    throw new \Exception($cellCoordinate . ' != ' . $result . ' result in sheet with name ' . $sheetName . ' is ' . ($calculatedValue ?? 'empty'));
+                }
+            }
+        }
+    }
 }
