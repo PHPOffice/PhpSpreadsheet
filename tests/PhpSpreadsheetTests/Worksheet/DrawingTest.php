4d3f3a7953b63324f95dd5af16d4e58a37278f84
Walter Nasich <wnasich@gmail.com>
2018-07-24 14:40:57 -0300

    Using a simpler xls file with images to test cloning worksheets with images

--- tests/PhpSpreadsheetTests/Worksheet/DrawingTest.php
+++ tests/PhpSpreadsheetTests/Worksheet/DrawingTest.php
@@ -2,17 +2,17 @@
 
 namespace PhpOffice\PhpSpreadsheetTests\Worksheet;
 
-use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+use PhpOffice\PhpSpreadsheet\Reader\Xls;
 use PHPUnit\Framework\TestCase;
 
 class DrawingTest extends TestCase
 {
     public function testCloningWorksheetWithImages()
     {
-        $filename = './data/Reader/XLSX/with_images.xlsx';
-        $reader = new Xlsx();
+        $filename = './data/Reader/Xls/with_images.xls';
+        $reader = new Xls();
         $spreadsheet = $reader->load($filename);
-        $worksheet = $spreadsheet->getSheetByName('Invoice');
+        $worksheet = $spreadsheet->getSheetByName('Sheet1');
         $originDrawingCount = count($worksheet->getDrawingCollection());
 
         $clonedWorksheet = clone $worksheet;
