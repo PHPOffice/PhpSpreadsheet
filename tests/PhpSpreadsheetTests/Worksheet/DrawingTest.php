0b63a16d80621bff1fbe6737f0551b5dd697f026
Walter Nasich <wnasich@gmail.com>
2018-07-28 16:56:23 -0300

    Test: using a spreadsheet in memory instead of load a xls file. Testing cloned objects exist and they are differents

--- tests/PhpSpreadsheetTests/Worksheet/DrawingTest.php
+++ tests/PhpSpreadsheetTests/Worksheet/DrawingTest.php
@@ -2,21 +2,39 @@
 
 namespace PhpOffice\PhpSpreadsheetTests\Worksheet;
 
-use PhpOffice\PhpSpreadsheet\Reader\Xls;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\MemoryDrawing;
 use PHPUnit\Framework\TestCase;
 
 class DrawingTest extends TestCase
 {
     public function testCloningWorksheetWithImages()
     {
-        $filename = './data/Reader/Xls/with_images.xls';
-        $reader = new Xls();
-        $spreadsheet = $reader->load($filename);
-        $worksheet = $spreadsheet->getSheetByName('Sheet1');
-        $originDrawingCount = count($worksheet->getDrawingCollection());
+        $spreadsheet = new Spreadsheet();
+        $aSheet = $spreadsheet->getActiveSheet();
 
-        $clonedWorksheet = clone $worksheet;
+        $gdImage = @imagecreatetruecolor(120, 20);
+        $textColor = imagecolorallocate($gdImage, 255, 255, 255);
+        imagestring($gdImage, 1, 5, 5, 'Created with PhpSpreadsheet', $textColor);
+
+        $drawing = new MemoryDrawing();
+        $drawing->setName('In-Memory image 1');
+        $drawing->setDescription('In-Memory image 1');
+        $drawing->setCoordinates('A1');
+        $drawing->setImageResource($gdImage);
+        $drawing->setRenderingFunction(
+            MemoryDrawing::RENDERING_JPEG
+        );
+        $drawing->setMimeType(MemoryDrawing::MIMETYPE_DEFAULT);
+        $drawing->setHeight(36);
+        $drawing->setWorksheet($aSheet);
+
+        $originDrawingCount = count($aSheet->getDrawingCollection());
+        $clonedWorksheet = clone $aSheet;
         $clonedDrawingCount = count($clonedWorksheet->getDrawingCollection());
+
         self::assertEquals($originDrawingCount, $clonedDrawingCount);
+        self::assertNotSame($aSheet, $clonedWorksheet);
+        self::assertNotSame($aSheet->getDrawingCollection(), $clonedWorksheet->getDrawingCollection());
     }
 }
