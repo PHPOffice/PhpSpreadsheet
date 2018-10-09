8e109cfb8c6110084b05528bc524ef6e68c280f7
marcusblevin <marcusblevin@gmail.com>
2018-10-09 09:37:01 -0400

    Change tabs to spaces

--- src/PhpSpreadsheet/Calculation/MathTrig.php
+++ src/PhpSpreadsheet/Calculation/MathTrig.php
@@ -1264,24 +1264,26 @@ class MathTrig
 
         // Loop through each sum and see if arguments and conditions are true
         foreach ($sumArgs as $index => $value) {
-        	$valid = true;
-        	
-        	foreach ($conditions as $cidx => $condition) {
-        		$arg = $aArgsArray[$cidx][$index];
-        		
-        		// Loop through arguments
-        		if (!is_numeric($arg)) {
-        			$arg = Calculation::wrapResult(strtoupper($arg));
-        		}
-        		$testCondition = '=' . $arg . $condition;
-        		if (!Calculation::getInstance()->_calculateFormulaValue($testCondition)) {
-        			// Is not a value within our criteria
-        			$valid = false;
+            $valid = true;
+
+            foreach ($conditions as $cidx => $condition) {
+                $arg = $aArgsArray[$cidx][$index];
+
+                // Loop through arguments
+                if (!is_numeric($arg)) {
+                    $arg = Calculation::wrapResult(strtoupper($arg));
+                }
+                $testCondition = '=' . $arg . $condition;
+                if (!Calculation::getInstance()->_calculateFormulaValue($testCondition)) {
+                    // Is not a value within our criteria
+                    $valid = false;
                     break;  // if false found, don't need to check other conditions
-        		}
-        	}
-        	
-        	if ($valid)  {  $returnValue += $value;  }
+                }
+            }
+
+            if ($valid)  {  
+                $returnValue += $value;  
+            }
         }
 
         // Return
