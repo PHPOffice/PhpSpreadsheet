PHPOffice/PhpSpreadsheet

commit 8dddf56c2e45ce7da0e41323929d0525f92329e2

Use proper syntax for variadic functions
This simplify code, increase readability and improve the function
signature for API users.

src/PhpSpreadsheet/Calculation.php
@@ -3051,9 +3051,9 @@ class Calculation
         return $formula;
     }
 
-    private static function mkMatrix()
+    private static function mkMatrix(...$args)
     {
-        return func_get_args();
+        return $args;
     }
 
     //    Binary Operators

commit 09eb05f367f7adf0b36869aa405eec84752b1377

OFFSET should allow omitted height and width
Commit 8dddf56 inadvertently removed the ability to omit the width
and height arguments to the OFFSET function. And #REF! is returned
because the function is validating that the new $pCell argument
is present. It is present, but it has been passed in the $height position.

We fixed this by always passing $pCell at the last position and filling
missing arguments with NULL values.

Fixes #561
Fixes #565

src/PhpSpreadsheet/Calculation/Calculation.php
@@ -3942,9 +3942,7 @@ class Calculation
                     }
 
                     //    Process the argument with the appropriate function call
-                    if ($passCellReference) {
-                        $args[] = $pCell;
-                    }
+                    $args = $this->addCellReference($args, $passCellReference, $pCell, $functionCall);
 
                     if (!is_array($functionCall)) {
                         foreach ($args as &$arg) {
@@ -4435,5 +4433,33 @@ class Calculation
         }
 
         return $returnValue;
+    }
+
+    /**
+     * Add cell reference if needed while making sure that it is the last argument.
+     *
+     * @param array $args
+     * @param bool $passCellReference
+     * @param Cell $pCell
+     * @param array $functionCall
+     *
+     * @return array
+     */
+    private function addCellReference(array $args, $passCellReference, Cell $pCell, array $functionCall)
+    {
+        if ($passCellReference) {
+            $className = $functionCall[0];
+            $methodName = $functionCall[1];
+
+            $reflectionMethod = new \ReflectionMethod($className, $methodName);
+            $argumentCount = count($reflectionMethod->getParameters());
+            while (count($args) < $argumentCount - 1) {
+                $args[] = null;
+            }
+
+            $args[] = $pCell;
+        }
+
+        return $args;
     }
 }

tests/PhpSpreadsheetTests/Calculation/CalculationTest.php
@@ -4,6 +4,7 @@ namespace PhpOffice\PhpSpreadsheetTests\Calculation;
 
 use PhpOffice\PhpSpreadsheet\Calculation\Calculation;
 use PhpOffice\PhpSpreadsheet\Calculation\Functions;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PHPUnit\Framework\TestCase;
 
 class CalculationTest extends TestCase
@@ -116,5 +117,27 @@ class CalculationTest extends TestCase
         self::assertCount(5, $tree);
         $function = $tree[4];
         self::assertEquals('Function', $function['type']);
+    }
+
+    public function testFormulaWithOptionalArgumentsAndRequiredCellReferenceShouldPassNullForMissingArguments()
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $sheet->fromArray(
+            [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9],
+            ]
+        );
+
+        $cell = $sheet->getCell('E5');
+        $cell->setValue('=OFFSET(D3, -1, -2, 1, 1)');
+        self::assertEquals(5, $cell->getCalculatedValue(), 'with all arguments');
+
+        $cell = $sheet->getCell('F6');
+        $cell->setValue('=OFFSET(D3, -1, -2)');
+        self::assertEquals(5, $cell->getCalculatedValue(), 'missing arguments should be filled with null');
     }
 }