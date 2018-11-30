147bd465837ec5f4804f766238b71a951728f832
Fräntz Miccoli <frantz.miccoli@fakedomain.com>
2018-11-30 12:23:33 +0100

    Working branch pruning with tests

--- tests/PhpSpreadsheetTests/Calculation/CalculationTest.php
+++ tests/PhpSpreadsheetTests/Calculation/CalculationTest.php
@@ -140,4 +140,103 @@ class CalculationTest extends TestCase
         $cell->setValue('=OFFSET(D3, -1, -2)');
         self::assertEquals(5, $cell->getCalculatedValue(), 'missing arguments should be filled with null');
     }
+
+    public function testBranchPruningFormulaParsing()
+    {
+        $calculation = Calculation::getInstance();
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $sheet->fromArray(
+            [
+                ['please +', 'please *', 'increment'],
+                [1, 1, 1], // sum is 3
+                [3, 3, 3], // product is 27
+            ]
+        );
+
+        $cell = $sheet->getCell('E5');
+
+        // Very simple formula
+        $formula = '=IF(A1="please +",B1)';
+        $tokens = $calculation->parseFormula($formula);
+
+        $foundEqualAssociatedToStoreKey = false;
+        $foundConditionalOnB1 = false;
+        foreach ($tokens as $token) {
+            $isBinaryOperator = $token['type'] == 'Binary Operator';
+            $isEqual = $token['value'] == '=';
+            $correctStoreKey = ($token['storeKey'] ?? '') == 'storeKey-0';
+            $correctOnlyIf = ($token['onlyIf'] ?? '') == 'storeKey-0';
+            $isB1Reference = ($token['reference'] ?? '') == 'B1';
+
+            $foundEqualAssociatedToStoreKey = $foundEqualAssociatedToStoreKey ||
+                ($isBinaryOperator && $isEqual && $correctStoreKey);
+
+            $foundConditionalOnB1 = $foundConditionalOnB1 ||
+                ($isB1Reference && $correctOnlyIf);
+        }
+        $this->assertTrue($foundEqualAssociatedToStoreKey);
+        $this->assertTrue($foundConditionalOnB1);
+
+        
+        $calculation->flushInstance(); // resets the ids
+
+        //
+        // Internal opeartio
+        $formula = '=IF(A1="please +",SUM(B1:B3))+IF(A2="please *",PRODUCT(C1:C3), C1)';
+        $tokens = $calculation->parseFormula($formula);
+
+        $plusGotTagged = false;
+        $productFunctionCorrectlyTagged = false;
+        foreach ($tokens as $token) {
+            $isBinaryOperator = $token['type'] == 'Binary Operator';
+            $isPlus = $token['value'] == '+';
+            $anyStoreKey = isset($token['storeKey']);
+            $anyOnlyIf = isset($token['onlyIf']);
+            $anyOnlyIfNot = isset($token['onlyIfNot']);
+            $plusGotTagged = $plusGotTagged ||
+                ($isBinaryOperator && $isPlus &&
+                    ($anyStoreKey || $anyOnlyIfNot || $anyOnlyIf));
+
+            $isFunction = $token['type'] == 'Function';
+            $isProductFunction = $token['value'] == 'PRODUCT(';
+            $correctOnlyIf = ($token['onlyIf'] ?? '') == 'storeKey-1';
+            $productFunctionCorrectlyTagged = $productFunctionCorrectlyTagged ||
+                ($isFunction && $isProductFunction && $correctOnlyIf);
+        }
+        $this->assertFalse($plusGotTagged,
+            'chaining IF( should not affect the external operators');
+        $this->assertTrue($productFunctionCorrectlyTagged,
+            'function nested inside if should be tagged to be processed only if parent branching requires it');
+
+        $calculation->flushInstance(); // resets the ids
+
+        $formula = '=IF(A1="please +",SUM(B1:B3),1+IF(NOT(A2="please *"),C2-C1,PRODUCT(C1:C3)))';
+        $tokens = $calculation->parseFormula($formula);
+
+        $plusCorrectlyTagged = false;
+        $productFunctionCorrectlyTagged = false;
+        $notFunctionCorrectlyTagged = false;
+        foreach($tokens as $token) {
+            $value = $token['value'];
+            $isPlus = $value == '+';
+            $isProductFunction = $value == 'PRODUCT(';
+            $isNotFunction = $value == 'NOT(';
+            $isOnlyIfNotDepth1 = $token['onlyIfNot'] == 'storeKey-1';
+            $isStoreKeyDepth1 = $token['storeKey'] == 'storeKey-1';
+            $isOnlyIfNotDepth0 = $token['onlyIfNot'] == 'storeKey-0';
+
+            $plusCorrectlyTagged = $plusCorrectlyTagged ||
+                ($isPlus && $isOnlyIfNotDepth0);
+            $notFunctionCorrectlyTagged = $notFunctionCorrectlyTagged ||
+                ($isNotFunction && $isOnlyIfNotDepth0 && $isStoreKeyDepth1);
+            $productFunctionCorrectlyTagged = $productFunctionCorrectlyTagged ||
+                ($isProductFunction && $isOnlyIfNotDepth1 && !$isStoreKeyDepth1 && !$isOnlyIfNotDepth0);
+        }
+        $this->assertTrue($plusCorrectlyTagged);
+        $this->assertTrue($productFunctionCorrectlyTagged);
+        $this->assertTrue($notFunctionCorrectlyTagged);
+    }
 }
