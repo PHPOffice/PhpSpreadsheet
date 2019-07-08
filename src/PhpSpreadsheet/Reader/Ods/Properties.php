094933164d199f701cb307fa706d5916648d1733
Mark Baker <mark@lange.demon.co.uk>
2019-07-08 22:35:08 +0200

    Ods reader fix for property reader when none set (#1064)
    * Fix for Ods Property Reader when no core properties are defined
    
    * Fix for Ods Property Reader when no meta properties are defined

--- src/PhpSpreadsheet/Reader/Ods/Properties.php
+++ src/PhpSpreadsheet/Reader/Ods/Properties.php
@@ -20,13 +20,13 @@ class Properties
         $officeProperty = $xml->children($namespacesMeta['office']);
         foreach ($officeProperty as $officePropertyData) {
             /** @var \SimpleXMLElement $officePropertyData */
-            $officePropertiesDC = [];
+            $officePropertiesDC = (object) [];
             if (isset($namespacesMeta['dc'])) {
                 $officePropertiesDC = $officePropertyData->children($namespacesMeta['dc']);
             }
             $this->setCoreProperties($docProps, $officePropertiesDC);
 
-            $officePropertyMeta = [];
+            $officePropertyMeta = (object) [];
             if (isset($namespacesMeta['dc'])) {
                 $officePropertyMeta = $officePropertyData->children($namespacesMeta['meta']);
             }
