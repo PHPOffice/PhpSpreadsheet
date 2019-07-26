f7d2ebac4027bf29007e347ab2f9c990ca787e0a
MarkBaker <mark@lange.demon.co.uk>
2019-07-27 00:10:43 +0200

    Minor fix to operator check

--- src/PhpSpreadsheet/Calculation/Calculation.php
+++ src/PhpSpreadsheet/Calculation/Calculation.php
@@ -3623,7 +3623,7 @@ class Calculation
 
                     //    If the last entry on the stack was a : operator, then we have a cell range reference
                     $testPrevOp = $stack->last(1);
-                    if ($testPrevOp['value'] == ':') {
+                    if ($testPrevOp !== null && $testPrevOp['value'] == ':') {
                         //    If we have a worksheet reference, then we're playing with a 3D reference
                         if ($matches[2] == '') {
                             //    Otherwise, we 'inherit' the worksheet reference from the start cell reference
