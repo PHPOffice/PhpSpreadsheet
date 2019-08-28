a59d6a3181db4b8329f7f5f323a8253d3420eba3
rolandsusans <rolands.usans@gmail.com>
2019-08-28 18:10:27 +0300

    set it off by default

--- tests/PhpSpreadsheetTests/Calculation/CalculateFromFileTest.php
+++ tests/PhpSpreadsheetTests/Calculation/CalculateFromFileTest.php
@@ -23,9 +23,6 @@ class CalculateFromFileTest extends TestCase
         $reader = IOFactory::createReader('Xlsx');
         $reader->setReadDataOnly(true);
         $spreadsheet = $reader->load(__DIR__ . '/../../data/Reader/XLSX/calculationTest.xlsx');
-        // turn off branch pruning
-        $cal = Calculation::getInstance($spreadsheet);
-        $cal->disableBranchPruning();
 
         $spreadsheet->setActiveSheetIndexByName('Sheet1');
         $spreadsheet->getActiveSheet()->setCellValue('B1', 'AL6 0JB');
@@ -48,15 +45,18 @@ class CalculateFromFileTest extends TestCase
         $reader = IOFactory::createReader('Xlsx');
         $reader->setReadDataOnly(true);
         $spreadsheet = $reader->load(__DIR__ . '/../../data/Reader/XLSX/calculationTest.xlsx');
+        // turn on branch pruning
+        $cal = Calculation::getInstance($spreadsheet);
+        $cal->enableBranchPruning();
 
         $spreadsheet->setActiveSheetIndexByName('Sheet1');
         $spreadsheet->getActiveSheet()->setCellValue('B1', 'AL6 0JB');
         if ($spreadsheet->getActiveSheet()->getCell('C1')->isFormula()) {
-            $this->assertEquals(true, $spreadsheet->getActiveSheet()->getCell('C1')->getCalculatedValue());
+            $this->assertEquals(false, $spreadsheet->getActiveSheet()->getCell('C1')->getCalculatedValue());
             $spreadsheet->setActiveSheetIndexByName('Sheet2');
             $this->assertEquals([$spreadsheet->getActiveSheet()->getCell('K11')->getCalculatedValue(),
                 $spreadsheet->getActiveSheet()->getCell('K12')->getCalculatedValue(),
-                $spreadsheet->getActiveSheet()->getCell('K13')->getCalculatedValue()], [15,16,22]);
+                $spreadsheet->getActiveSheet()->getCell('K13')->getCalculatedValue()], ['Pruned branch (only if storeKey-2) K3','Pruned branch (only if storeKey-3) K7','Pruned branch (only if storeKey-4) K9']);
         }
     }
 }
