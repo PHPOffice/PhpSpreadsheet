1f6482c379607b30a1013a615cdad784643442c4
MarkBaker <mark@lange.demon.co.uk>
2019-10-28 20:19:51 +0100

    Verify locale unit tests with a different locale setting

--- tests/PhpSpreadsheetTests/Calculation/Functions/TextData/FixedTest.php
+++ tests/PhpSpreadsheetTests/Calculation/Functions/TextData/FixedTest.php
@@ -40,4 +40,28 @@ class FixedTest extends TestCase
     {
         return require 'data/Calculation/TextData/FIXED.php';
     }
+
+    /**
+     * @dataProvider providerFIXEDLocale
+     *
+     * @param mixed $expectedResult
+     */
+    public function testFIXEDLocale($expectedResult, ...$args)
+    {
+        $locale = setlocale(LC_CTYPE, 0);
+        $localeSet = setlocale(LC_ALL, 'fr_FR.UTF8');
+        if ($localeSet === false) {
+            $this->markTestSkipped('Unable to set locale for FIXED() test.');
+        }
+
+        $result = TextData::FIXEDFORMAT(...$args);
+        $this->assertEquals($expectedResult, $result);
+
+        setlocale(LC_ALL, $locale);
+    }
+
+    public function providerFIXEDLocale()
+    {
+        return require 'data/Calculation/TextData/FIXEDLocale.php';
+    }
 }
