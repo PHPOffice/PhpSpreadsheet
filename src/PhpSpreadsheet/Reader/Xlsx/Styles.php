e5409f0fed230b8cfe8aeb8a587c29322bd06e46
Roland Eigelsreiter <brainfoolong@users.noreply.github.com>
2019-10-29 10:24:35 +0100

    fix access error
    Fix the error "Trying to access array offset on value of type null"

--- src/PhpSpreadsheet/Reader/Xlsx/Styles.php
+++ src/PhpSpreadsheet/Reader/Xlsx/Styles.php
@@ -163,7 +163,7 @@ class Styles extends BaseParserClass
             self::readBorderStyle($docStyle->getBorders(), $style->border);
         }
 
-        if (isset($style->alignment)) {
+        if (isset($style->alignment->alignment)) {
             self::readAlignmentStyle($docStyle->getAlignment(), $style->alignment);
         }
 
