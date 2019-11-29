01b710bfa5b29c5b1d3a55a8afff63b8b757a8cc
Owen Leibman <eclipsechasers2@yahoo.com>
2019-11-29 13:32:19 -0800

    Improved Test
    Try/catch wasn't needed. Phpunit catches the unexpected exception.

--- tests/PhpSpreadsheetTests/Calculation/DefinedNameConfusedForCellTest.php
+++ tests/PhpSpreadsheetTests/Calculation/DefinedNameConfusedForCellTest.php
@@ -6,6 +6,22 @@ use PHPUnit\Framework\TestCase;
 
 class DefinedNameConfusedForCellTest extends TestCase
 {
+    const FILENAM = 'calcerror';
+    const TYP = 'Xlsx';
+
+    private static function getfilename()
+    {
+        return self::FILENAM . '.' . strtolower(self::TYP);
+    }
+
+    public function tearDown()
+    {
+        $out = self::getfilename();
+        if (file_exists($out)) {
+            unlink($out);
+        }
+    }
+
     public function testDefinedName()
     {
         $obj = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
@@ -13,16 +29,9 @@ class DefinedNameConfusedForCellTest extends TestCase
         $sheet0->setCellValue('A1', 2);
         $obj->addNamedRange(new \PhpOffice\PhpSpreadsheet\NamedRange('A1A', $sheet0, 'A1'));
         $sheet0->setCellValue('B1', '=2*A1A');
-        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($obj, 'Xlsx');
-        $out = 'calcerror.xlsx';
-
-        try {
-            $writer->save($out);
-        } catch (\Exception $e) {
-            unlink($out);
-            $this->fail();
-        }
-        unlink($out);
-        $this->assertTrue(true);
+        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($obj, self::TYP);
+        $out = self::getfilename();
+        $writer->save($out);
+        self::assertTrue(true);
     }
 }
