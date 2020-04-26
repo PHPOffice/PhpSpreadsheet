8732c190b694ce99439e05c74416d7933aeb53c3
Adrien Crivelli <adrien.crivelli@gmail.com>
2020-04-26 21:29:37 +0900

    Update src/PhpSpreadsheet/Calculation/Functions.php

--- src/PhpSpreadsheet/Calculation/Functions.php
+++ src/PhpSpreadsheet/Calculation/Functions.php
@@ -646,7 +646,7 @@ class Functions
     public static function flattenSingleValue($value = '')
     {
         while (is_array($value)) {
-            $value = array_shift($value); // was array_pop
+            $value = array_shift($value);
         }
 
         return $value;
