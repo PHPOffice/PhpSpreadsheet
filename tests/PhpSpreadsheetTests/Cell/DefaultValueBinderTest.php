eb18b3376cc50cd7e5b5902e33edff1cd8c064a9
Gianni Genovesi <giannign1@gmail.com>
2020-05-23 11:30:20 +0200

    style: missing terminal comma in array list

--- tests/PhpSpreadsheetTests/Cell/DefaultValueBinderTest.php
+++ tests/PhpSpreadsheetTests/Cell/DefaultValueBinderTest.php
@@ -57,7 +57,7 @@ class DefaultValueBinderTest extends TestCase
             ['#REF!'],
             [new DateTime()],
             [new DateTimeImmutable()],
-            ['123456\n']
+            ['123456\n'],
         ];
     }
 
