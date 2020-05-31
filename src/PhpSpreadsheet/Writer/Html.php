bbaf03c2efec48720c67dbaa8f3b2aab6a112786
oleibman <eclipsechasers2@yahoo.com>
2020-05-31 11:18:47 -0700

    Update Html.php
    Travis says no need to initialize private class variable to null.

--- src/PhpSpreadsheet/Writer/Html.php
+++ src/PhpSpreadsheet/Writer/Html.php
@@ -136,7 +136,7 @@ class Html extends BaseWriter
      *
      * @var null|callable
      */
-    protected $editHtmlCallback = null;
+    protected $editHtmlCallback;
 
     /**
      * Create a new HTML.
