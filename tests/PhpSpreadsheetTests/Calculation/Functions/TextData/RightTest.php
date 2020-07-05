fcc72d619323b06ae4eede790775e6494e42b394
Owen Leibman <eclipsechasers2@yahoo.com>
2020-07-05 09:57:42 -0700

    Restoring State After Static Changes in Tests
    This request does not change any source code, only tests.
    
    For a change on which I was working, a test passed when run on its own,
    but failed when run as part of the full test suite. It turned out that
    an existing test had changed a static value,
    thousands separator in this case, and failed to restore it.
    The test turned out to be AdvancedBinderTest.
    
    The search for the offending test was more difficult than it should have
    been because 26 test scripts which had nothing to do with thousands
    separator nevertheless changed that value. They all changed
    decimal separator, currency code, and compatibility mode as well,
    again for no reason. I changed all of those to eliminate those operations.
    
    I changed the following tests, which actually do change the static
    properties identified above for a reason, to restore them as part of teardown.
    - CalculationTest sets compatibilityMode and locale
    - DayTest sets compatibilityMode, returnDateType, and excelCalendar
    - CountTest sets compatibilityMode
    - FunctionsTest sets compatibilityMode and returnDateType
    - AdvancedValueBinderTest sets currencyCode, decimalSeparator, thousandsSeparator
    - StringHelperTest sets currencyCode, decimalSeparator, thousandsSeparator
    - NumberFormatTest sets currencyCode, decimalSeparator, thousandsSeparator
    - HtmlNumberFormatTest sets currencyCode, decimalSeparator, thousandsSeparator

--- tests/PhpSpreadsheetTests/Calculation/Functions/TextData/RightTest.php
+++ tests/PhpSpreadsheetTests/Calculation/Functions/TextData/RightTest.php
@@ -2,29 +2,11 @@
 
 namespace PhpOffice\PhpSpreadsheetTests\Calculation\Functions\TextData;
 
-use PhpOffice\PhpSpreadsheet\Calculation\Functions;
 use PhpOffice\PhpSpreadsheet\Calculation\TextData;
-use PhpOffice\PhpSpreadsheet\Shared\StringHelper;
 use PHPUnit\Framework\TestCase;
 
 class RightTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        Functions::setCompatibilityMode(Functions::COMPATIBILITY_EXCEL);
-        StringHelper::setDecimalSeparator('.');
-        StringHelper::setThousandsSeparator(',');
-        StringHelper::setCurrencyCode('$');
-    }
-
-    protected function tearDown(): void
-    {
-        Functions::setCompatibilityMode(Functions::COMPATIBILITY_EXCEL);
-        StringHelper::setDecimalSeparator('.');
-        StringHelper::setThousandsSeparator(',');
-        StringHelper::setCurrencyCode('$');
-    }
-
     /**
      * @dataProvider providerRIGHT
      *
