b129d21df4eeb2e665a76849b47490e643ce39bd
MarkBaker <mark@lange.demon.co.uk>
2021-03-10 11:26:02 +0100

    Start refactoring the actual method code to improve readability and reduce some redundancy

--- src/PhpSpreadsheet/Calculation/LookupRef/Lookup.php
+++ src/PhpSpreadsheet/Calculation/LookupRef/Lookup.php
@@ -29,75 +29,44 @@ class Lookup
      * The VLOOKUP function searches for value in the left-most column of lookup_array and returns the value
      *     in the same row based on the index_number.
      *
-     * @param mixed $lookup_value The value that you want to match in lookup_array
-     * @param mixed $lookup_array The range of cells being searched
-     * @param mixed $index_number The column number in table_array from which the matching value must be returned.
+     * @param mixed $lookupValue The value that you want to match in lookup_array
+     * @param mixed $lookupArray The range of cells being searched
+     * @param mixed $indexNumber The column number in table_array from which the matching value must be returned.
      *                                The first column is 1.
-     * @param mixed $not_exact_match determines if you are looking for an exact match based on lookup_value
+     * @param mixed $notExactMatch determines if you are looking for an exact match based on lookup_value
      *
      * @return mixed The value of the found cell
      */
-    public static function VLOOKUP($lookup_value, $lookup_array, $index_number, $not_exact_match = true)
+    public static function VLOOKUP($lookupValue, $lookupArray, $indexNumber, $notExactMatch = true)
     {
-        $lookup_value = Functions::flattenSingleValue($lookup_value);
-        $index_number = Functions::flattenSingleValue($index_number);
-        $not_exact_match = Functions::flattenSingleValue($not_exact_match);
+        $lookupValue = Functions::flattenSingleValue($lookupValue);
+        $indexNumber = Functions::flattenSingleValue($indexNumber);
+        $notExactMatch = Functions::flattenSingleValue($notExactMatch);
 
         try {
-            $index_number = self::validateIndexLookup($lookup_array, $index_number);
+            $indexNumber = self::validateIndexLookup($lookupArray, $indexNumber);
         } catch (Exception $e) {
             return $e->getMessage();
         }
 
-        $f = array_keys($lookup_array);
+        $f = array_keys($lookupArray);
         $firstRow = array_pop($f);
-        if ((!is_array($lookup_array[$firstRow])) || ($index_number > count($lookup_array[$firstRow]))) {
+        if ((!is_array($lookupArray[$firstRow])) || ($indexNumber > count($lookupArray[$firstRow]))) {
             return Functions::REF();
         }
-        $columnKeys = array_keys($lookup_array[$firstRow]);
-        $returnColumn = $columnKeys[--$index_number];
+        $columnKeys = array_keys($lookupArray[$firstRow]);
+        $returnColumn = $columnKeys[--$indexNumber];
         $firstColumn = array_shift($columnKeys);
 
-        if (!$not_exact_match) {
-            uasort($lookup_array, ['self', 'vlookupSort']);
+        if (!$notExactMatch) {
+            uasort($lookupArray, ['self', 'vlookupSort']);
         }
 
-        $lookupLower = StringHelper::strToLower($lookup_value);
-        $rowNumber = $rowValue = false;
-        foreach ($lookup_array as $rowKey => $rowData) {
-            $firstLower = StringHelper::strToLower($rowData[$firstColumn]);
+        $rowNumber = self::vLookupSearch($lookupValue, $lookupArray, $firstColumn, $notExactMatch);
 
-            // break if we have passed possible keys
-            if (
-                (is_numeric($lookup_value) && is_numeric($rowData[$firstColumn]) && ($rowData[$firstColumn] > $lookup_value)) ||
-                (!is_numeric($lookup_value) && !is_numeric($rowData[$firstColumn]) && ($firstLower > $lookupLower))
-            ) {
-                break;
-            }
-            // remember the last key, but only if datatypes match
-            if (
-                (is_numeric($lookup_value) && is_numeric($rowData[$firstColumn])) ||
-                (!is_numeric($lookup_value) && !is_numeric($rowData[$firstColumn]))
-            ) {
-                if ($not_exact_match) {
-                    $rowNumber = $rowKey;
-
-                    continue;
-                } elseif (
-                    ($firstLower == $lookupLower)
-                    // Spreadsheets software returns first exact match,
-                    // we have sorted and we might have broken key orders
-                    // we want the first one (by its initial index)
-                    && (($rowNumber == false) || ($rowKey < $rowNumber))
-                ) {
-                    $rowNumber = $rowKey;
-                }
-            }
-        }
-
-        if ($rowNumber !== false) {
+        if ($rowNumber !== null) {
             // return the appropriate value
-            return $lookup_array[$rowNumber][$returnColumn];
+            return $lookupArray[$rowNumber][$returnColumn];
         }
 
         return Functions::NA();
@@ -108,70 +77,40 @@ class Lookup
      * The HLOOKUP function searches for value in the top-most row of lookup_array and returns the value
      *     in the same column based on the index_number.
      *
-     * @param mixed $lookup_value The value that you want to match in lookup_array
-     * @param mixed $lookup_array The range of cells being searched
-     * @param mixed $index_number The row number in table_array from which the matching value must be returned.
+     * @param mixed $lookupValue The value that you want to match in lookup_array
+     * @param mixed $lookupArray The range of cells being searched
+     * @param mixed $indexNumber The row number in table_array from which the matching value must be returned.
      *                                The first row is 1.
-     * @param mixed $not_exact_match determines if you are looking for an exact match based on lookup_value
+     * @param mixed $notExactMatch determines if you are looking for an exact match based on lookup_value
      *
      * @return mixed The value of the found cell
      */
-    public static function HLOOKUP($lookup_value, $lookup_array, $index_number, $not_exact_match = true)
+    public static function HLOOKUP($lookupValue, $lookupArray, $indexNumber, $notExactMatch = true)
     {
-        $lookup_value = Functions::flattenSingleValue($lookup_value);
-        $index_number = Functions::flattenSingleValue($index_number);
-        $not_exact_match = Functions::flattenSingleValue($not_exact_match);
+        $lookupValue = Functions::flattenSingleValue($lookupValue);
+        $indexNumber = Functions::flattenSingleValue($indexNumber);
+        $notExactMatch = Functions::flattenSingleValue($notExactMatch);
 
         try {
-            $index_number = self::validateIndexLookup($lookup_array, $index_number);
+            $indexNumber = self::validateIndexLookup($lookupArray, $indexNumber);
         } catch (Exception $e) {
             return $e->getMessage();
         }
 
-        $f = array_keys($lookup_array);
+        $f = array_keys($lookupArray);
         $firstRow = reset($f);
-        if ((!is_array($lookup_array[$firstRow])) || ($index_number > count($lookup_array))) {
+        if ((!is_array($lookupArray[$firstRow])) || ($indexNumber > count($lookupArray))) {
             return Functions::REF();
         }
 
         $firstkey = $f[0] - 1;
-        $returnColumn = $firstkey + $index_number;
+        $returnColumn = $firstkey + $indexNumber;
         $firstColumn = array_shift($f);
-        $rowNumber = null;
-        foreach ($lookup_array[$firstColumn] as $rowKey => $rowData) {
-            // break if we have passed possible keys
-            $bothNumeric = is_numeric($lookup_value) && is_numeric($rowData);
-            $bothNotNumeric = !is_numeric($lookup_value) && !is_numeric($rowData);
-            $lookupLower = StringHelper::strToLower($lookup_value);
-            $rowDataLower = StringHelper::strToLower($rowData);
-
-            if (
-                $not_exact_match && (
-                    ($bothNumeric && $rowData > $lookup_value) ||
-                    ($bothNotNumeric && $rowDataLower > $lookupLower)
-                )
-            ) {
-                break;
-            }
-
-            // Remember the last key, but only if datatypes match (as in VLOOKUP)
-            if ($bothNumeric || $bothNotNumeric) {
-                if ($not_exact_match) {
-                    $rowNumber = $rowKey;
-
-                    continue;
-                } elseif (
-                    $rowDataLower === $lookupLower
-                    && ($rowNumber === null || $rowKey < $rowNumber)
-                ) {
-                    $rowNumber = $rowKey;
-                }
-            }
-        }
+        $rowNumber = self::hLookupSearch($lookupValue, $lookupArray, $firstColumn, $notExactMatch);
 
         if ($rowNumber !== null) {
             //  otherwise return the appropriate value
-            return $lookup_array[$returnColumn][$rowNumber];
+            return $lookupArray[$returnColumn][$rowNumber];
         }
 
         return Functions::NA();
@@ -181,54 +120,51 @@ class Lookup
      * LOOKUP
      * The LOOKUP function searches for value either from a one-row or one-column range or from an array.
      *
-     * @param mixed $lookup_value The value that you want to match in lookup_array
-     * @param mixed $lookup_vector The range of cells being searched
-     * @param null|mixed $result_vector The column from which the matching value must be returned
+     * @param mixed $lookupValue The value that you want to match in lookup_array
+     * @param mixed $lookupVector The range of cells being searched
+     * @param null|mixed $resultVector The column from which the matching value must be returned
      *
      * @return mixed The value of the found cell
      */
-    public static function LOOKUP($lookup_value, $lookup_vector, $result_vector = null)
+    public static function LOOKUP($lookupValue, $lookupVector, $resultVector = null)
     {
-        $lookup_value = Functions::flattenSingleValue($lookup_value);
+        $lookupValue = Functions::flattenSingleValue($lookupValue);
 
-        if (!is_array($lookup_vector)) {
+        if (!is_array($lookupVector)) {
             return Functions::NA();
         }
-        $hasResultVector = isset($result_vector);
-        $lookupRows = count($lookup_vector);
-        $l = array_keys($lookup_vector);
+        $hasResultVector = isset($resultVector);
+        $lookupRows = count($lookupVector);
+        $l = array_keys($lookupVector);
         $l = array_shift($l);
-        $lookupColumns = count($lookup_vector[$l]);
+        $lookupColumns = count($lookupVector[$l]);
         // we correctly orient our results
         if (($lookupRows === 1 && $lookupColumns > 1) || (!$hasResultVector && $lookupRows === 2 && $lookupColumns !== 2)) {
-            $lookup_vector = LookupRef::TRANSPOSE($lookup_vector);
-            $lookupRows = count($lookup_vector);
-            $l = array_keys($lookup_vector);
-            $lookupColumns = count($lookup_vector[array_shift($l)]);
+            $lookupVector = LookupRef::TRANSPOSE($lookupVector);
+            $lookupRows = count($lookupVector);
+            $l = array_keys($lookupVector);
+            $lookupColumns = count($lookupVector[array_shift($l)]);
         }
 
-        if ($result_vector === null) {
-            $result_vector = $lookup_vector;
+        if ($resultVector === null) {
+            $resultVector = $lookupVector;
         }
-        $resultRows = count($result_vector);
-        $l = array_keys($result_vector);
+        $resultRows = count($resultVector);
+        $l = array_keys($resultVector);
         $l = array_shift($l);
-        $resultColumns = count($result_vector[$l]);
+        $resultColumns = count($resultVector[$l]);
         // we correctly orient our results
         if ($resultRows === 1 && $resultColumns > 1) {
-            $result_vector = LookupRef::TRANSPOSE($result_vector);
-            $resultRows = count($result_vector);
-            $r = array_keys($result_vector);
-            $resultColumns = count($result_vector[array_shift($r)]);
+            $resultVector = LookupRef::TRANSPOSE($resultVector);
         }
 
         if ($lookupRows === 2 && !$hasResultVector) {
-            $result_vector = array_pop($lookup_vector);
-            $lookup_vector = array_shift($lookup_vector);
+            $resultVector = array_pop($lookupVector);
+            $lookupVector = array_shift($lookupVector);
         }
 
         if ($lookupColumns !== 2) {
-            foreach ($lookup_vector as &$value) {
+            foreach ($lookupVector as &$value) {
                 if (is_array($value)) {
                     $k = array_keys($value);
                     $key1 = $key2 = array_shift($k);
@@ -239,7 +175,7 @@ class Lookup
                     $key2 = 1;
                     $dataValue1 = $value;
                 }
-                $dataValue2 = array_shift($result_vector);
+                $dataValue2 = array_shift($resultVector);
                 if (is_array($dataValue2)) {
                     $dataValue2 = array_shift($dataValue2);
                 }
@@ -248,7 +184,7 @@ class Lookup
             unset($value);
         }
 
-        return self::VLOOKUP($lookup_value, $lookup_vector, 2);
+        return self::VLOOKUP($lookupValue, $lookupVector, 2);
     }
 
     private static function vlookupSort($a, $b)
@@ -262,5 +198,69 @@ class Lookup
         }
 
         return ($aLower < $bLower) ? -1 : 1;
+    }
+
+    private static function vLookupSearch($lookupValue, $lookupArray, $column, $notExactMatch)
+    {
+        $lookupLower = StringHelper::strToLower($lookupValue);
+
+        $rowNumber = null;
+        foreach ($lookupArray as $rowKey => $rowData) {
+            $bothNumeric = is_numeric($lookupValue) && is_numeric($rowData[$column]);
+            $bothNotNumeric = !is_numeric($lookupValue) && !is_numeric($rowData[$column]);
+            $firstLower = StringHelper::strToLower($rowData[$column]);
+
+            // break if we have passed possible keys
+            if (
+                (is_numeric($lookupValue) && is_numeric($rowData[$column]) && ($rowData[$column] > $lookupValue)) ||
+                (!is_numeric($lookupValue) && !is_numeric($rowData[$column]) && ($firstLower > $lookupLower))
+            ) {
+                break;
+            }
+
+            // remember the last key, but only if datatypes match
+            if ($bothNumeric || $bothNotNumeric) {
+                // Spreadsheets software returns first exact match,
+                // we have sorted and we might have broken key orders
+                // we want the first one (by its initial index)
+                if ($notExactMatch) {
+                    $rowNumber = $rowKey;
+                } elseif (($firstLower == $lookupLower) && (($rowNumber == false) || ($rowKey < $rowNumber))) {
+                    $rowNumber = $rowKey;
+                }
+            }
+        }
+
+        return $rowNumber;
+    }
+
+    private static function hLookupSearch($lookupValue, $lookupArray, $column, $notExactMatch)
+    {
+        $lookupLower = StringHelper::strToLower($lookupValue);
+
+        $rowNumber = null;
+        foreach ($lookupArray[$column] as $rowKey => $rowData) {
+            // break if we have passed possible keys
+            $bothNumeric = is_numeric($lookupValue) && is_numeric($rowData);
+            $bothNotNumeric = !is_numeric($lookupValue) && !is_numeric($rowData);
+            $rowDataLower = StringHelper::strToLower($rowData);
+
+            if ($notExactMatch &&
+                (($bothNumeric && $rowData > $lookupValue) || ($bothNotNumeric && $rowDataLower > $lookupLower))
+            ) {
+                break;
+            }
+
+            // Remember the last key, but only if datatypes match (as in VLOOKUP)
+            if ($bothNumeric || $bothNotNumeric) {
+                if ($notExactMatch) {
+                    $rowNumber = $rowKey;
+                } elseif ($rowDataLower === $lookupLower && ($rowNumber === null || $rowKey < $rowNumber)) {
+                    $rowNumber = $rowKey;
+                }
+            }
+        }
+
+        return $rowNumber;
     }
 }
