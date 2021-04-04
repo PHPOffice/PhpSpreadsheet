42761f90b7b3ecd5e319cf330129a8b588abb2b5
Mark Baker <mark@lange.demon.co.uk>
2021-04-04 14:44:06 +0200

    Financial start refactoring cash flow functions (#1986)
    * Start extracting CashFlow functions from Financial, beginning with the simple Single Rate flows
    * Extracting Variable Periodic and NonPeriodic CashFlow functions from Financial
    * Some more unit tests for exception cases

--- src/PhpSpreadsheet/Calculation/Financial/CashFlow/Single.php
+++ src/PhpSpreadsheet/Calculation/Financial/CashFlow/Single.php
@@ -2,6 +2,110 @@
 
 namespace PhpOffice\PhpSpreadsheet\Calculation\Financial\CashFlow;
 
+use PhpOffice\PhpSpreadsheet\Calculation\Exception;
+use PhpOffice\PhpSpreadsheet\Calculation\Financial\BaseValidations;
+use PhpOffice\PhpSpreadsheet\Calculation\Functions;
+
 class Single
 {
+    use BaseValidations;
+
+    /**
+     * FVSCHEDULE.
+     *
+     * Returns the future value of an initial principal after applying a series of compound interest rates.
+     * Use FVSCHEDULE to calculate the future value of an investment with a variable or adjustable rate.
+     *
+     * Excel Function:
+     *        FVSCHEDULE(principal,schedule)
+     *
+     * @param mixed $principal the present value
+     * @param float[] $schedule an array of interest rates to apply
+     *
+     * @return float|string
+     */
+    public static function futureValue($principal, $schedule)
+    {
+        $principal = Functions::flattenSingleValue($principal);
+        $schedule = Functions::flattenArray($schedule);
+
+        try {
+            $principal = self::validateFloat($principal);
+
+            foreach ($schedule as $rate) {
+                $rate = self::validateFloat($rate);
+                $principal *= 1 + $rate;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+        return $principal;
+    }
+
+    /**
+     * PDURATION.
+     *
+     * Calculates the number of periods required for an investment to reach a specified value.
+     *
+     * @param float $rate Interest rate per period
+     * @param float $presentValue Present Value
+     * @param float $futureValue Future Value
+     *
+     * @return float|string Result, or a string containing an error
+     */
+    public static function periods($rate = 0.0, $presentValue = 0.0, $futureValue = 0.0)
+    {
+        $rate = Functions::flattenSingleValue($rate);
+        $presentValue = Functions::flattenSingleValue($presentValue);
+        $futureValue = Functions::flattenSingleValue($futureValue);
+
+        try {
+            $rate = self::validateFloat($rate);
+            $presentValue = self::validateFloat($presentValue);
+            $futureValue = self::validateFloat($futureValue);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+        // Validate parameters
+        if ($rate <= 0.0 || $presentValue <= 0.0 || $futureValue <= 0.0) {
+            return Functions::NAN();
+        }
+
+        return (log($futureValue) - log($presentValue)) / log(1 + $rate);
+    }
+
+    /**
+     * RRI.
+     *
+     * Calculates the interest rate required for an investment to grow to a specified future value .
+     *
+     * @param float $periods The number of periods over which the investment is made
+     * @param float $presentValue Present Value
+     * @param float $futureValue Future Value
+     *
+     * @return float|string Result, or a string containing an error
+     */
+    public static function interestRate($periods = 0.0, $presentValue = 0.0, $futureValue = 0.0)
+    {
+        $periods = Functions::flattenSingleValue($periods);
+        $presentValue = Functions::flattenSingleValue($presentValue);
+        $futureValue = Functions::flattenSingleValue($futureValue);
+
+        try {
+            $periods = self::validateFloat($periods);
+            $presentValue = self::validateFloat($presentValue);
+            $futureValue = self::validateFloat($futureValue);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+        // Validate parameters
+        if ($periods <= 0.0 || $presentValue <= 0.0 || $futureValue < 0.0) {
+            return Functions::NAN();
+        }
+
+        return ($futureValue / $presentValue) ** (1 / $periods) - 1;
+    }
 }
