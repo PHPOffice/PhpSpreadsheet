e962ba5fdff4997774f9b4ad890e771ee79fc1dc
MarkBaker <mark@lange.demon.co.uk>
2021-04-05 23:13:29 +0200

    More validation checks and unit tests

--- src/PhpSpreadsheet/Calculation/Financial/CashFlow/Constant/Periodic/Payments.php
+++ src/PhpSpreadsheet/Calculation/Financial/CashFlow/Constant/Periodic/Payments.php
@@ -2,20 +2,24 @@
 
 namespace PhpOffice\PhpSpreadsheet\Calculation\Financial\CashFlow\Constant\Periodic;
 
+use PhpOffice\PhpSpreadsheet\Calculation\Exception;
+use PhpOffice\PhpSpreadsheet\Calculation\Financial\BaseValidations;
 use PhpOffice\PhpSpreadsheet\Calculation\Functions;
 
 class Payments
 {
+    use BaseValidations;
+
     /**
      * PMT.
      *
      * Returns the constant payment (annuity) for a cash flow with a constant interest rate.
      *
-     * @param float $interestRate Interest rate per period
-     * @param int $numberOfPeriods Number of periods
-     * @param float $presentValue Present Value
-     * @param float $futureValue Future Value
-     * @param int $type Payment type: 0 = at the end of each period, 1 = at the beginning of each period
+     * @param mixed $interestRate Interest rate per period
+     * @param mixed $numberOfPeriods Number of periods
+     * @param mixed $presentValue Present Value
+     * @param mixed $futureValue Future Value
+     * @param mixed $type Payment type: 0 = at the end of each period, 1 = at the beginning of each period
      *
      * @return float|string Result, or a string containing an error
      */
@@ -27,13 +31,23 @@ class Payments
         $futureValue = Functions::flattenSingleValue($futureValue);
         $type = Functions::flattenSingleValue($type);
 
+        try {
+            $interestRate = self::validateFloat($interestRate);
+            $numberOfPeriods = self::validateInt($numberOfPeriods);
+            $presentValue = self::validateFloat($presentValue);
+            $futureValue = self::validateFloat($futureValue);
+            $type = self::validateInt($type);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
         // Validate parameters
         if ($type != 0 && $type != 1) {
             return Functions::NAN();
         }
 
         // Calculate
-        if ($interestRate !== null && $interestRate != 0) {
+        if ($interestRate != 0.0) {
             return (-$futureValue - $presentValue * (1 + $interestRate) ** $numberOfPeriods) /
                 (1 + $interestRate * $type) / (((1 + $interestRate) ** $numberOfPeriods - 1) / $interestRate);
         }
@@ -47,23 +61,34 @@ class Payments
      * Returns the interest payment for a given period for an investment based on periodic, constant payments
      *         and a constant interest rate.
      *
-     * @param float $interestRate Interest rate per period
-     * @param int $period Period for which we want to find the interest
-     * @param int $numberOfPeriods Number of periods
-     * @param float $presentValue Present Value
-     * @param float $futureValue Future Value
-     * @param int $type Payment type: 0 = at the end of each period, 1 = at the beginning of each period
+     * @param mixed $interestRate Interest rate per period
+     * @param mixed $period Period for which we want to find the interest
+     * @param mixed $numberOfPeriods Number of periods
+     * @param mixed $presentValue Present Value
+     * @param mixed $futureValue Future Value
+     * @param mixed $type Payment type: 0 = at the end of each period, 1 = at the beginning of each period
      *
      * @return float|string Result, or a string containing an error
      */
     public static function PPMT($interestRate, $period, $numberOfPeriods, $presentValue, $futureValue = 0, $type = 0)
     {
         $interestRate = Functions::flattenSingleValue($interestRate);
-        $period = (int) Functions::flattenSingleValue($period);
-        $numberOfPeriods = (int) Functions::flattenSingleValue($numberOfPeriods);
+        $period = Functions::flattenSingleValue($period);
+        $numberOfPeriods = Functions::flattenSingleValue($numberOfPeriods);
         $presentValue = Functions::flattenSingleValue($presentValue);
-        $futureValue = Functions::flattenSingleValue($futureValue);
-        $type = (int) Functions::flattenSingleValue($type);
+        $futureValue = ($futureValue === null) ? 0.0 : Functions::flattenSingleValue($futureValue);
+        $type = ($type === null) ? 0 : Functions::flattenSingleValue($type);
+
+        try {
+            $interestRate = self::validateFloat($interestRate);
+            $period = self::validateInt($period);
+            $numberOfPeriods = self::validateInt($numberOfPeriods);
+            $presentValue = self::validateFloat($presentValue);
+            $futureValue = self::validateFloat($futureValue);
+            $type = self::validateInt($type);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
 
         // Validate parameters
         if ($type != 0 && $type != 1) {
