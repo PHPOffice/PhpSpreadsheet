f6bfbd065524e7805c209e1478a8053e7f235aa5
Akira Taniguchi <akira.penguin@gmail.com>
2021-04-06 02:56:54 +0900

    Create CsvOutputEncodingTest.php

---
+++ tests/PhpSpreadsheetTests/Writer/Csv/CsvOutputEncodingTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PhpOffice\PhpSpreadsheetTests\Writer\Csv;
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Csv as CsvWriter;
+use PhpOffice\PhpSpreadsheet\Writer\Exception as WriterException;
+use PhpOffice\PhpSpreadsheetTests\Functional;
+
+class CsvWriteTest extends Functional\AbstractFunctional
+{
+    public function testEncoding(): void
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'こんにちは！');
+        $sheet->setCellValue('B1', 'Hello!');
+
+        $filename = File::sysGetTempDir(), 'phpspreadsheet-test-UTF-8';
+        $writer->useBOM(false);
+        $writer->setOutputEncoding('');
+        $writer->save($filename);
+        $a = file_get_contents($filename);
+        unlink($filename);
+        
+        $filename = File::sysGetTempDir(), 'phpspreadsheet-test-SJIS-win';
+        $writer->useBOM(false);
+        $writer->setOutputEncoding('SJIS-win');
+        $writer->save($filename);
+        $b = file_get_contents($filename);
+        unlink($filename);
+        
+        self::assertEquals(mb_convert_encoding($a, 'SJIS-win'), $b);
+    }
+}
