fe934ebb13a7c9491d2e24826b1ea907464ad25d
Owen Leibman <eclipsechasers2@yahoo.com>
2021-04-18 11:36:27 -0700

    Resolve Differences in Matrix Test Functions
    Upper vs lowercase names.

--- tests/PhpSpreadsheetTests/Calculation/Functions/MathTrig/MUnitTest.php
+++ tests/PhpSpreadsheetTests/Calculation/Functions/MathTrig/MUnitTest.php
@@ -11,13 +11,13 @@ class MUnitTest extends AllSetupTeardown
         $identity = MatrixFunctions::Identity(3);
         self::assertEquals([[1, 0, 0], [0, 1, 0], [0, 0, 1]], $identity);
         $startArray = [[1, 2, 2], [4, 5, 6], [7, 8, 9]];
-        $resultArray = MatrixFunctions::Multiply($startArray, $identity);
+        $resultArray = MatrixFunctions::multiply($startArray, $identity);
         self::assertEquals($startArray, $resultArray);
-        $inverseArray = MatrixFunctions::Inverse($startArray);
-        $resultArray = MatrixFunctions::Multiply($startArray, $inverseArray);
+        $inverseArray = MatrixFunctions::inverse($startArray);
+        $resultArray = MatrixFunctions::multiply($startArray, $inverseArray);
         self::assertEquals($identity, $resultArray);
-        self::assertEquals('#VALUE!', MatrixFunctions::Identity(0));
-        self::assertEquals('#VALUE!', MatrixFunctions::Identity(-1));
-        self::assertEquals('#VALUE!', MatrixFunctions::Identity('X'));
+        self::assertEquals('#VALUE!', MatrixFunctions::identity(0));
+        self::assertEquals('#VALUE!', MatrixFunctions::identity(-1));
+        self::assertEquals('#VALUE!', MatrixFunctions::identity('X'));
     }
 }
