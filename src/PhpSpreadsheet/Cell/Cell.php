2d5cfde4d746daf366fdb4edc6bf0b37e85fb43a
Owen Leibman <eclipsechasers2@yahoo.com>
2021-04-19 11:16:41 -0700

    Changed Expected Error Message Text Between PHP7 and PHP8
    Accept either old or new text.

--- src/PhpSpreadsheet/Cell/Cell.php
+++ src/PhpSpreadsheet/Cell/Cell.php
@@ -268,9 +268,7 @@ class Cell
             } catch (Exception $ex) {
                 if (($ex->getMessage() === 'Unable to access External Workbook') && ($this->calculatedValue !== null)) {
                     return $this->calculatedValue; // Fallback for calculations referencing external files.
-                } elseif (strpos($ex->getMessage(), 'undefined name') !== false) {
-                    return \PhpOffice\PhpSpreadsheet\Calculation\Functions::NAME();
-                } elseif (strpos($ex->getMessage(), 'Undefined offset') !== false) {
+                } elseif (preg_match('/[Uu]ndefined (name|offset: 2|array key 2)/', $ex->getMessage()) === 1) {
                     return \PhpOffice\PhpSpreadsheet\Calculation\Functions::NAME();
                 }
 
