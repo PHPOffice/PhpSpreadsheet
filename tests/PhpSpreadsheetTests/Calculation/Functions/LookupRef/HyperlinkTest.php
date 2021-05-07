85f4ce73887efc1acc0cdb036d6cd3fe77585d1c
MarkBaker <mark@lange.demon.co.uk>
2021-05-07 23:22:28 +0200

    Keep phpstan happy

--- tests/PhpSpreadsheetTests/Calculation/Functions/LookupRef/HyperlinkTest.php
+++ tests/PhpSpreadsheetTests/Calculation/Functions/LookupRef/HyperlinkTest.php
@@ -14,6 +14,7 @@ class HyperlinkTest extends TestCase
      * @dataProvider providerHYPERLINK
      *
      * @param mixed $expectedResult
+     * @param null|string $linkUrl
      * @param null|string $description
      */
     public function testHYPERLINK($expectedResult, $linkUrl, $description): void
