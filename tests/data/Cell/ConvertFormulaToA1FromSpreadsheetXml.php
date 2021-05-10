a7c992f89632ff52495e48b1bfbc65302f6cc344
Nathan Dench <ndenc2@gmail.com>
2021-05-11 09:53:03 +1000

    Track down bug in AddressHelper::convertFormulaToA1

--- tests/data/Cell/ConvertFormulaToA1FromSpreadsheetXml.php
+++ tests/data/Cell/ConvertFormulaToA1FromSpreadsheetXml.php
@@ -1,7 +1,16 @@
 <?php
 
 return [
-    ['=B4+C4', 'of:=[.B4]+[.C4]'],
-    ['=SUM(B1:B4)', 'of:=SUM([.B1:.B4])'],
-    ['=CONCAT("Result of formula expression =[.B4]+[.C4] is: ", B4+C4)', 'of:=CONCAT("Result of formula expression =[.B4]+[.C4] is: ", [.B4]+[.C4])'],
+    // Basic arithmetic
+    ['=D3+F7+G4+C6+5', 'of:=[.D3]+[.F7]+[.G4]+[.C6]+5'],
+    ['=D3-F7-G4-C6-5', 'of:=[.D3]-[.F7]-[.G4]-[.C6]-5'],
+    ['=D3*F7*G4*C6*5', 'of:=[.D3]*[.F7]*[.G4]*[.C6]*5'],
+    ['=D3/F7/G4/C6/5', 'of:=[.D3]/[.F7]/[.G4]/[.C6]/5'],
+    // Formulas
+    ['=SUM(E1:E5)', 'of:=SUM([.E1:.E5])'],
+    ['=SUM(E1:E5, D5)', 'of:=SUM([.E1:.E5], [.D5])'],
+    ['=SUM(E1:E5, D5)-C5', 'of:=SUM([.E1:.E5], [.D5])-[.C5]'],
+    ['=IF(E1>E2, E3, E4)', 'of:=IF([.E1]>[.E2], [.E3], [.E4])'],
+    // String literals
+    ['=CONCAT("Result of formula expression =[.C3]+[.C4] is: ", C3+C4)', 'of:=CONCAT("Result of formula expression =[.C3]+[.C4] is: ", [.C3]+[.C4])'],
 ];
