191122a084d7691d3b2d540e236951a19f59333a
oleibman <eclipsechasers2@yahoo.com>
2021-05-11 07:16:13 -0700

    Update Namespaces.php
    Mark Baker expressed preference for single strings rather than concatenations here.

--- src/PhpSpreadsheet/Reader/Xlsx/Namespaces.php
+++ src/PhpSpreadsheet/Reader/Xlsx/Namespaces.php
@@ -6,53 +6,53 @@ class Namespaces
 {
     const SCHEMAS = 'http://schemas.openxmlformats.org';
 
-    const RELATIONSHIPS = self::SCHEMAS . '/package/2006/relationships';
+    const RELATIONSHIPS = 'http://schemas.openxmlformats.org/package/2006/relationships';
 
     // This one used in Reader\Xlsx
-    const CORE_PROPERTIES = self::RELATIONSHIPS . '/metadata/core-properties';
+    const CORE_PROPERTIES = 'http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties';
 
     // This one used in Reader\Xlsx\Properties
-    const CORE_PROPERTIES2 = self::SCHEMAS . '/package/2006/metadata/core-properties';
+    const CORE_PROPERTIES2 = 'http://schemas.openxmlformats.org/package/2006/metadata/core-properties';
 
-    const THEME = self::RELATIONSHIPS . '/theme';
+    const THEME = 'http://schemas.openxmlformats.org/package/2006/relationships/theme';
 
-    const COMPATIBILITY = self::SCHEMAS . '/markup-compatibility/2006';
+    const COMPATIBILITY = 'http://schemas.openxmlformats.org/markup-compatibility/2006';
 
-    const MAIN = self::SCHEMAS . '/spreadsheetml/2006/main';
+    const MAIN = 'http://schemas.openxmlformats.org/spreadsheetml/2006/main';
 
-    const DRAWINGML = self::SCHEMAS . '/drawingml/2006/main';
+    const DRAWINGML = 'http://schemas.openxmlformats.org/drawingml/2006/main';
 
-    const CHART = self::SCHEMAS . '/drawingml/2006/chart';
+    const CHART = 'http://schemas.openxmlformats.org/drawingml/2006/chart';
 
-    const SPREADSHEET_DRAWING = self::SCHEMAS . '/drawingml/2006/spreadsheetDrawing';
+    const SPREADSHEET_DRAWING = 'http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing';
 
-    const SCHEMA_OFFICE_DOCUMENT = self::SCHEMAS . '/officeDocument/2006/relationships';
+    const SCHEMA_OFFICE_DOCUMENT = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships';
 
-    const COMMENTS = self::SCHEMA_OFFICE_DOCUMENT . '/comments';
+    const COMMENTS = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/comments';
 
-    //const CUSTOM_PROPERTIES = self::SCHEMA_OFFICE_DOCUMENT . '/custom-properties';
+    //const CUSTOM_PROPERTIES = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/custom-properties';
 
-    //const EXTENDED_PROPERTIES = self::SCHEMA_OFFICE_DOCUMENT . '/extended-properties';
+    //const EXTENDED_PROPERTIES = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties';
 
-    const HYPERLINK = self::SCHEMA_OFFICE_DOCUMENT . '/hyperlink';
+    const HYPERLINK = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink';
 
-    const OFFICE_DOCUMENT = self::SCHEMA_OFFICE_DOCUMENT . '/officeDocument';
+    const OFFICE_DOCUMENT = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument';
 
-    const SHARED_STRINGS = self::SCHEMA_OFFICE_DOCUMENT . '/sharedStrings';
+    const SHARED_STRINGS = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings';
 
-    const STYLES = self::SCHEMA_OFFICE_DOCUMENT . '/styles';
+    const STYLES = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles';
 
-    const IMAGE = self::SCHEMA_OFFICE_DOCUMENT . '/image';
+    const IMAGE = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/image';
 
-    const VML = self::SCHEMA_OFFICE_DOCUMENT . '/vmlDrawing';
+    const VML = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/vmlDrawing';
 
-    const WORKSHEET = self::SCHEMA_OFFICE_DOCUMENT . '/worksheet';
+    const WORKSHEET = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet';
 
     const SCHEMA_MICROSOFT = 'http://schemas.microsoft.com/office/2006/relationships';
 
-    const EXTENSIBILITY = self::SCHEMA_MICROSOFT . '/ui/extensibility';
+    const EXTENSIBILITY = 'http://schemas.microsoft.com/office/2006/relationships/ui/extensibility';
 
-    const VBA = self::SCHEMA_MICROSOFT . '/vbaProject';
+    const VBA = 'http://schemas.microsoft.com/office/2006/relationships/vbaProject';
 
     const DC_ELEMENTS = 'http://purl.org/dc/elements/1.1/';
 
@@ -64,13 +64,13 @@ class Namespaces
 
     const SCHEMA_PURL = 'http://purl.oclc.org/ooxml';
 
-    const PURL_OFFICE_DOCUMENT = self::SCHEMA_PURL . '/officeDocument/relationships/officeDocument';
+    const PURL_OFFICE_DOCUMENT = 'http://purl.oclc.org/ooxml/officeDocument/relationships/officeDocument';
 
-    const PURL_RELATIONSHIPS = self::SCHEMA_PURL . '/officeDocument/relationships';
+    const PURL_RELATIONSHIPS = 'http://purl.oclc.org/ooxml/officeDocument/relationships';
 
-    const PURL_MAIN = self::SCHEMA_PURL . '/spreadsheetml/main';
+    const PURL_MAIN = 'http://purl.oclc.org/ooxml/spreadsheetml/main';
 
-    const PURL_DRAWING = self::SCHEMA_PURL . '/drawingml/main';
+    const PURL_DRAWING = 'http://purl.oclc.org/ooxml/drawingml/main';
 
-    const PURL_WORKSHEET = self::PURL_RELATIONSHIPS . '/worksheet';
+    const PURL_WORKSHEET = 'http://purl.oclc.org/ooxml/officeDocument/relationships/worksheet';
 }
