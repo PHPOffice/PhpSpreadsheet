7b853f9a65221280254f7737a351874cad5c7d25
Owen Leibman <eclipsechasers2@yahoo.com>
2021-05-13 19:02:11 -0700

    Minor Improvement to Test Cleanup LookupRef
    Permit spreadsheet allocated as private member in test class to be garbage-collected after test completion.

--- tests/PhpSpreadsheetTests/Calculation/Functions/LookupRef/ColumnOnSpreadsheetTest.php
+++ tests/PhpSpreadsheetTests/Calculation/Functions/LookupRef/ColumnOnSpreadsheetTest.php
@@ -15,12 +15,12 @@ class ColumnOnSpreadsheetTest extends AllSetupTeardown
     public function testColumnOnSpreadsheet($expectedResult, $cellReference = 'omitted'): void
     {
         $this->mightHaveException($expectedResult);
-        $sheet = $this->sheet;
-        $this->spreadsheet->addNamedRange(new NamedRange('namedrangex', $sheet, '$E$2:$E$6'));
-        $this->spreadsheet->addNamedRange(new NamedRange('namedrangey', $sheet, '$F$2:$H$2'));
-        $this->spreadsheet->addNamedRange(new NamedRange('namedrange3', $sheet, '$F$4:$H$4'));
+        $sheet = $this->getSheet();
+        $this->getSpreadsheet()->addNamedRange(new NamedRange('namedrangex', $sheet, '$E$2:$E$6'));
+        $this->getSpreadsheet()->addNamedRange(new NamedRange('namedrangey', $sheet, '$F$2:$H$2'));
+        $this->getSpreadsheet()->addNamedRange(new NamedRange('namedrange3', $sheet, '$F$4:$H$4'));
 
-        $sheet1 = $this->spreadsheet->createSheet();
+        $sheet1 = $this->getSpreadsheet()->createSheet();
         $sheet1->setTitle('OtherSheet');
 
         if ($cellReference === 'omitted') {
@@ -40,11 +40,11 @@ class ColumnOnSpreadsheetTest extends AllSetupTeardown
 
     public function testCOLUMNLocalDefinedName(): void
     {
-        $sheet = $this->sheet;
+        $sheet = $this->getSheet();
 
-        $sheet1 = $this->spreadsheet->createSheet();
+        $sheet1 = $this->getSpreadsheet()->createSheet();
         $sheet1->setTitle('OtherSheet');
-        $this->spreadsheet->addNamedRange(new NamedRange('newnr', $sheet1, '$F$5:$H$5', true)); // defined locally, only usable on sheet1
+        $this->getSpreadsheet()->addNamedRange(new NamedRange('newnr', $sheet1, '$F$5:$H$5', true)); // defined locally, only usable on sheet1
 
         $sheet1->getCell('B3')->setValue('=COLUMN(newnr)');
         $result = $sheet1->getCell('B3')->getCalculatedValue();
