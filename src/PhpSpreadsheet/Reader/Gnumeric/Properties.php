189c72e185b702d6253bac9589f6c4186bd4ac0b
Owen Leibman <eclipsechasers2@yahoo.com>
2021-05-30 09:29:03 -0700

    Gnumeric Reader - Distinguish Created and Modified Timestamps
    Both are being used to set both fields; change to set the appropriate one in each case.
    
    Also replace use of 32-bit-unsafe strtotime.

--- src/PhpSpreadsheet/Reader/Gnumeric/Properties.php
+++ src/PhpSpreadsheet/Reader/Gnumeric/Properties.php
@@ -78,9 +78,7 @@ class Properties
 
                     break;
                 case 'date':
-                    $creationDate = strtotime($propertyValue);
-                    $creationDate = $creationDate === false ? time() : $creationDate;
-                    $docProps->setCreated($creationDate);
+                    $creationDate = $propertyValue;
                     $docProps->setModified($creationDate);
 
                     break;
@@ -110,10 +108,8 @@ class Properties
 
                         break;
                     case 'creation-date':
-                        $creationDate = strtotime($propertyValue);
-                        $creationDate = $creationDate === false ? time() : $creationDate;
+                        $creationDate = $propertyValue;
                         $docProps->setCreated($creationDate);
-                        $docProps->setModified($creationDate);
 
                         break;
                     case 'user-defined':
