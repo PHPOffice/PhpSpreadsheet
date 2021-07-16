0e54cf8b17fa20ae794fbd9c93a332d944ec3353
James Lucas <james@lucas.net.au>
2021-07-16 13:41:12 +1000

    * Fix reading data validation flags allowBlank, showDropDown, showInputMessage, showErrorMessage in XLSX reader (was a loose comparison of SimpleXML object to integer), flag values may also be string true/false not just 0/1.   filter_var( $flag, FILTER_VALIDATE_BOOLEAN) chosen to handle both 0/1 and true/false values in the file being read. * Fix writing data validation flag showDropDown (Inverted logic in reader was replicated to Writer)

--- src/PhpSpreadsheet/Reader/Xlsx/DataValidations.php
+++ src/PhpSpreadsheet/Reader/Xlsx/DataValidations.php
@@ -34,10 +34,10 @@ class DataValidations
                     $docValidation->setType((string) $dataValidation['type']);
                     $docValidation->setErrorStyle((string) $dataValidation['errorStyle']);
                     $docValidation->setOperator((string) $dataValidation['operator']);
-                    $docValidation->setAllowBlank($dataValidation['allowBlank'] != 0);
-                    $docValidation->setShowDropDown($dataValidation['showDropDown'] == 0);
-                    $docValidation->setShowInputMessage($dataValidation['showInputMessage'] != 0);
-                    $docValidation->setShowErrorMessage($dataValidation['showErrorMessage'] != 0);
+                    $docValidation->setAllowBlank(filter_var($dataValidation['allowBlank'], FILTER_VALIDATE_BOOLEAN));
+                    $docValidation->setShowDropDown(filter_var($dataValidation['showDropDown'], FILTER_VALIDATE_BOOLEAN));
+                    $docValidation->setShowInputMessage(filter_var($dataValidation['showInputMessage'], FILTER_VALIDATE_BOOLEAN));
+                    $docValidation->setShowErrorMessage(filter_var($dataValidation['showErrorMessage'], FILTER_VALIDATE_BOOLEAN));
                     $docValidation->setErrorTitle((string) $dataValidation['errorTitle']);
                     $docValidation->setError((string) $dataValidation['error']);
                     $docValidation->setPromptTitle((string) $dataValidation['promptTitle']);
