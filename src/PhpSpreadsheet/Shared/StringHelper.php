c2c6ca2cdb7cb7c08a1e96f39525525daf9cf0f8
Roland Eigelsreiter <brainfoolong@users.noreply.github.com>
2021-09-08 11:11:38 +0200

    fixed null conversation for strToUpper
    fixed in the same way as it already has been done for strToLower

--- src/PhpSpreadsheet/Shared/StringHelper.php
+++ src/PhpSpreadsheet/Shared/StringHelper.php
@@ -490,7 +490,7 @@ class StringHelper
      */
     public static function strToUpper($pValue)
     {
-        return mb_convert_case($pValue, MB_CASE_UPPER, 'UTF-8');
+        return mb_convert_case($pValue ?? '', MB_CASE_UPPER, 'UTF-8');
     }
 
     /**
