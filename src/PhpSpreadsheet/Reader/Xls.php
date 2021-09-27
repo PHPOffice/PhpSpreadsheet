04e8f8da8e315f223d0815cf938fea51912a193c
georgio <georgiokot@gmail.cim>
2021-09-28 00:52:41 +0300

    fix #1114 issues

--- src/PhpSpreadsheet/Reader/Xls.php
+++ src/PhpSpreadsheet/Reader/Xls.php
@@ -3749,12 +3749,11 @@ class Xls extends BaseReader
                         } else {
                             $textRun = $richText->createTextRun($text);
                             if (isset($fmtRuns[$i - 1])) {
-                                if ($fmtRuns[$i - 1]['fontIndex'] < 4) {
-                                    $fontIndex = $fmtRuns[$i - 1]['fontIndex'];
-                                } else {
-                                    // this has to do with that index 4 is omitted in all BIFF versions for some strange reason
-                                    // check the OpenOffice documentation of the FONT record
-                                    $fontIndex = $fmtRuns[$i - 1]['fontIndex'] - 1;
+                                $fontIndex = $fmtRuns[$i - 1]['fontIndex'];
+                                // this has to do with that index 3 and 4  is omitted in all BIFF versions for some strange reason
+                                // check the OpenOffice documentation of the FONT record
+                                if (array_key_exists($fontIndex,$this->objFonts) === false) {
+                                    $fontIndex = array_key_last($this->objFonts);
                                 }
                                 $textRun->setFont(clone $this->objFonts[$fontIndex]);
                             }
