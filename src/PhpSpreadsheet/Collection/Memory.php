99ed19b69271680ed0c4f2af0ab36a601ca6a56d
jmsche <contact@jmsche.fr>
2021-12-17 18:12:00 +0100

    Fix PHP 8.1 deprecations

--- src/PhpSpreadsheet/Collection/Memory.php
+++ src/PhpSpreadsheet/Collection/Memory.php
@@ -14,6 +14,9 @@ class Memory implements CacheInterface
 {
     private $cache = [];
 
+    /**
+     * @return bool
+     */
     public function clear()
     {
         $this->cache = [];
@@ -21,6 +24,11 @@ class Memory implements CacheInterface
         return true;
     }
 
+    /**
+     * @param string $key
+     *
+     * @return bool
+     */
     public function delete($key)
     {
         unset($this->cache[$key]);
@@ -28,6 +36,11 @@ class Memory implements CacheInterface
         return true;
     }
 
+    /**
+     * @param iterable $keys
+     *
+     * @return bool
+     */
     public function deleteMultiple($keys)
     {
         foreach ($keys as $key) {
@@ -37,6 +50,12 @@ class Memory implements CacheInterface
         return true;
     }
 
+    /**
+     * @param string $key
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
     public function get($key, $default = null)
     {
         if ($this->has($key)) {
@@ -46,6 +65,12 @@ class Memory implements CacheInterface
         return $default;
     }
 
+    /**
+     * @param iterable $keys
+     * @param mixed    $default
+     *
+     * @return iterable
+     */
     public function getMultiple($keys, $default = null)
     {
         $results = [];
@@ -56,11 +81,23 @@ class Memory implements CacheInterface
         return $results;
     }
 
+    /**
+     * @param string $key
+     *
+     * @return bool
+     */
     public function has($key)
     {
         return array_key_exists($key, $this->cache);
     }
 
+    /**
+     * @param string                 $key
+     * @param mixed                  $value
+     * @param null|int|\DateInterval $ttl
+     *
+     * @return bool
+     */
     public function set($key, $value, $ttl = null)
     {
         $this->cache[$key] = $value;
@@ -68,6 +105,12 @@ class Memory implements CacheInterface
         return true;
     }
 
+    /**
+     * @param iterable               $values
+     * @param null|int|\DateInterval $ttl
+     *
+     * @return bool
+     */
     public function setMultiple($values, $ttl = null)
     {
         foreach ($values as $key => $value) {
