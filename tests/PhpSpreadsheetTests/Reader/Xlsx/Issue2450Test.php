3a6558625dcc40e558cc176f2d3efe0dae429197
oleibman <eclipsechasers2@yahoo.com>
2021-12-18 09:25:08 -0800

    General Style Specified in Uppercase in Input Xlsx (#2451)
    * General Style Specified in Uppercase in Input Xlsx
    
    Fix #2450. Treat input style GENERAL as if it were expected upper/lowercase.
    
    * Declare Method as Static
    
    Surprised neither Phpstan nor Scrutinizer flagged this.
    
    * Remove Duplicated Statement
    
    Don't know why Scrutinizer didn't flag this the first time.

---
+++ tests/PhpSpreadsheetTests/Reader/Xlsx/Issue2450Test.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PhpOffice\PhpSpreadsheetTests\Reader\Xlsx;
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PHPUnit\Framework\TestCase;
+
+class Issue2450Test extends TestCase
+{
+    public function testIssue2450(): void
+    {
+        // Style specified as GENERAL rather than General.
+        $filename = 'tests/data/Reader/XLSX/issue.2450.xlsx';
+        $reader = IOFactory::createReader('Xlsx');
+        $spreadsheet = $reader->load($filename);
+        $sheet = $spreadsheet->getActiveSheet();
+        $birthYears = [
+            'C2' => $sheet->getCell('C2')->getFormattedValue(),
+            'C3' => $sheet->getCell('C3')->getFormattedValue(),
+            'C4' => $sheet->getCell('C4')->getFormattedValue(),
+        ];
+        self::assertSame(
+            [
+                'C2' => '1932',
+                'C3' => '1964',
+                'C4' => '1988',
+            ],
+            $birthYears
+        );
+
+        $spreadsheet->disconnectWorksheets();
+    }
+}
