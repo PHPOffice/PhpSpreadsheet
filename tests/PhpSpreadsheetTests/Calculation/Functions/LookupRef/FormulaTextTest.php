0e7b0e155b7d93d486044280f1ad1d9b318142db
Owen Leibman <owenleibman@fico.com>
2021-12-25 07:55:45 -0800

    Replace Tests With Unneeded Mocking
    Replace mock tests with real ones when possible. The original tests are all still present; they just take place in a more representative scenario.
    
    After this, there will be 4 remaining uses of mocking. Of these, 3 are needed for scenarios which are otherwise hard to test - WebServiceTest, CellsTest, and SampleCoverageTest. For the other one, AutoFilterTest, I just can't figure out what it's trying to accomplish, so have left it alone.
    
    This change is almost entirely restricted to tests. There is a one-line change in src. When the first argument passed to OFFSET is null or nullstring, the returned value is currently 0. However, according to the documentation for Excel, it should be `#VALUE!`. The code is changed accordingly.

---
+++ tests/PhpSpreadsheetTests/Calculation/Functions/LookupRef/FormulaTextTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PhpOffice\PhpSpreadsheetTests\Calculation\Functions\LookupRef;
+
+use PhpOffice\PhpSpreadsheet\Calculation\LookupRef\Formula;
+
+/**
+ * Class FormulaTextTest.
+ */
+class FormulaTextTest extends AllSetupTeardown
+{
+    /**
+     * @param mixed $value
+     * @dataProvider providerFormulaText
+     */
+    public function testFormulaText(string $expectedResult, $value): void
+    {
+        $sheet = $this->getSheet();
+        $reference = 'A1';
+        if ($value !== null) {
+            $sheet->getCell($reference)->setValue($value);
+        }
+        $sheet->getCell('D1')->setValue("=FORMULATEXT($reference)");
+        $result = $sheet->getCell('D1')->getCalculatedValue();
+        self::assertSame($expectedResult, $result);
+    }
+
+    public function providerFormulaText(): array
+    {
+        return require 'tests/data/Calculation/LookupRef/FORMULATEXT.php';
+    }
+
+    public function testNoCell(): void
+    {
+        self::assertSame('#REF!', Formula::text('B5'));
+    }
+}
