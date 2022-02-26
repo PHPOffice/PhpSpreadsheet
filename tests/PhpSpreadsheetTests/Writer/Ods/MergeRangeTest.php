444d0fd77c1c466cb7485d8fbabc4e625d6ab1e5
MarkBaker <mark@lange.demon.co.uk>
2022-02-26 21:41:37 +0100

    Unit tests for merge ranges for Ods Reader/Writer

---
+++ tests/PhpSpreadsheetTests/Writer/Ods/MergeRangeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PhpOffice\PhpSpreadsheetTests\Writer\Ods;
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheetTests\Functional\AbstractFunctional;
+
+class MergeRangeTest extends AbstractFunctional
+{
+    public function testMergeRangeWriter(): void
+    {
+        $mergeRange = 'B2:C3';
+        $spreadsheet = new Spreadsheet();
+        $worksheet = $spreadsheet->getActiveSheet();
+        $worksheet->setCellValue('B2', "Merge Range {$mergeRange}");
+        $worksheet->mergeCells($mergeRange);
+
+        $reloaded = $this->writeAndReload($spreadsheet, 'Ods');
+
+        $cell = $reloaded->getActiveSheet()->getCell('B2');
+        self::assertTrue($cell->isInMergeRange());
+        self::assertSame($mergeRange, $cell->getMergeRange());
+    }
+}
