e4b7d73a9a7ccbfe35634bb5e5663392c893313f
MarkBaker <mark@lange.demon.co.uk>
2022-03-04 22:08:10 +0100

    Unit tests for reading array formulae in Ods files

--- tests/PhpSpreadsheetTests/Reader/Ods/ArrayFormulaTest.php
+++ tests/PhpSpreadsheetTests/Reader/Ods/ArrayFormulaTest.php
@@ -25,16 +25,14 @@ class ArrayFormulaTest extends TestCase
         $worksheet = $this->spreadsheet->getActiveSheet();
 
         $cell = $worksheet->getCell('B2');
-//        self::assertTrue($cell->isArrayFormula());
-//        self::assertSame('B2:C3', $cell->arrayFormulaRange());
-//        self::assertSame('={2,3}*{4;5}', $cell->getValue());
-//        var_dump($worksheet->getCell('B2')->getValue());
-////        var_dump($worksheet->getCell('B2')->getCalculatedValue(true, true)); // Is currently resetting the range to NULLs
-//        var_dump($worksheet->getCell('C2')->getValue());
-////        var_dump($worksheet->getCell('C2')->getCalculatedValue());
-//        var_dump($worksheet->getCell('B3')->getValue());
-////        var_dump($worksheet->getCell('B3')->getCalculatedValue());
-//        var_dump($worksheet->getCell('C3')->getValue());
-////        var_dump($worksheet->getCell('C3')->getCalculatedValue());
+        self::assertTrue($cell->isArrayFormula());
+        self::assertSame('B2:C3', $cell->arrayFormulaRange());
+        self::assertSame('={2,3}*{4;5}', $cell->getValue());
+        self::assertSame([[8, 12], [10, 15]], $cell->getCalculatedValue(true, true));
+        self::assertSame(8, $cell->getCalculatedValue());
+        self::assertSame(8, $cell->getCalculatedValue());
+        self::assertSame(12, $worksheet->getCell('C2')->getCalculatedValue());
+        self::assertSame(10, $worksheet->getCell('B3')->getCalculatedValue());
+        self::assertSame(15, $worksheet->getCell('C3')->getCalculatedValue());
     }
 }
