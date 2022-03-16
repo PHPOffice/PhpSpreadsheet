9428552d9446eca65bdaf2f98a90f4709af8a882
oleibman <10341515+oleibman@users.noreply.github.com>
2022-03-16 16:12:38 -0700

    Add editAs Property for 2-cell Anchor Drawings (#2674)
    * Add editAs Property for 2-cell Anchor Drawings
    
    This change builds on PR #2532 (@naotake51 as PR #2532), using ideas from PR #2237 (@AdamGaskins), which has had changes requested for several months. It covers a lot of the same ground as 2532. In Excel, two-cell anchor drawings can be edited as "twocell", "onecell", or "absolute". This PR adds support for those options, with a sample file that demonstrates the difference in addition to unit tests. Several other tests are added to improve the spotty coverage for Drawings.
    
    There have been several other tickets referencing two cell anchors, including issue #1159 and PR #1160 (@sgarwood, who also added support for editAs), PR #643, and issue #126, all now closed but not necessarily entirely resolved. I will try to ensure that those tickets are addressed with this one.
    
    And, in trying to make sure 1160 is covered, I stumbled upon a bug. If you use the same image resource to create two+ memory drawings, the MemoryDrawing destructor for the first will cause the rest to generate a very long warning message. This is not a problem for Php8+, only for Php7-. I have suppressed the message in the MemoryDrawing constructor. 1160 went stale due to an unresolved test error, but I don't think this was the problem. At any rate, its test works now.
    
    * Scrutinizer
    
    It reported 1 minor issue (fixed normally), and 2 major. One is fixed with a kludge. The other is a case where Scrutinizer's analysis is just wrong, and I can't figure out a kludge. But I was able to add an annotation (the first time I've managed to get one past phpcs/php-cs-fixer). We'll see.

--- tests/PhpSpreadsheetTests/Worksheet/DrawingTest.php
+++ tests/PhpSpreadsheetTests/Worksheet/DrawingTest.php
@@ -2,7 +2,10 @@
 
 namespace PhpOffice\PhpSpreadsheetTests\Worksheet;
 
+use PhpOffice\PhpSpreadsheet\Exception as PhpSpreadsheetException;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use PhpOffice\PhpSpreadsheet\Worksheet\HeaderFooterDrawing;
 use PhpOffice\PhpSpreadsheet\Worksheet\MemoryDrawing;
 use PHPUnit\Framework\TestCase;
 
@@ -40,5 +43,77 @@ class DrawingTest extends TestCase
             self::assertNotSame($aSheet->getDrawingCollection(), $clonedWorksheet->getDrawingCollection());
             $spreadsheet->disconnectWorksheets();
         }
+    }
+
+    public function testChangeWorksheet(): void
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet1 = $spreadsheet->getActiveSheet();
+        $sheet2 = $spreadsheet->createSheet();
+
+        $drawing = new Drawing();
+        $drawing->setName('Green Square');
+        $drawing->setPath('tests/data/Writer/XLSX/green_square.gif');
+        self::assertEquals($drawing->getWidth(), 150);
+        self::assertEquals($drawing->getHeight(), 150);
+        $drawing->setCoordinates('A1');
+        $drawing->setOffsetX(30);
+        $drawing->setOffsetY(10);
+        $drawing->setWorksheet($sheet1);
+
+        try {
+            $drawing->setWorksheet($sheet2);
+            self::fail('Should throw exception when attempting set worksheet without specifying override');
+        } catch (PhpSpreadsheetException $e) {
+            self::assertStringContainsString('A Worksheet has already been assigned.', $e->getMessage());
+        }
+        self::assertSame($sheet1, $drawing->getWorksheet());
+        self::assertCount(1, $sheet1->getDrawingCollection());
+        self::assertCount(0, $sheet2->getDrawingCollection());
+        $drawing->setWorksheet($sheet2, true);
+        self::assertSame($sheet2, $drawing->getWorksheet());
+        self::assertCount(0, $sheet1->getDrawingCollection());
+        self::assertCount(1, $sheet2->getDrawingCollection());
+    }
+
+    public function testHeaderFooter(): void
+    {
+        $drawing1 = new HeaderFooterDrawing();
+        $drawing1->setName('Blue Square');
+        $drawing1->setPath('tests/data/Writer/XLSX/blue_square.png');
+        self::assertEquals($drawing1->getWidth(), 100);
+        self::assertEquals($drawing1->getHeight(), 100);
+        $drawing2 = new HeaderFooterDrawing();
+        $drawing2->setName('Blue Square');
+        $drawing2->setPath('tests/data/Writer/XLSX/blue_square.png');
+        self::assertSame($drawing1->getHashCode(), $drawing2->getHashCode());
+        $drawing2->setOffsetX(100);
+        self::assertNotEquals($drawing1->getHashCode(), $drawing2->getHashCode());
+    }
+
+    public function testSetWidthAndHeight(): void
+    {
+        $drawing = new Drawing();
+        $drawing->setName('Blue Square');
+        $drawing->setPath('tests/data/Writer/XLSX/blue_square.png');
+        self::assertSame(100, $drawing->getWidth());
+        self::assertSame(100, $drawing->getHeight());
+        self::assertTrue($drawing->getResizeProportional());
+        $drawing->setResizeProportional(false);
+        $drawing->setWidthAndHeight(150, 200);
+        self::assertSame(150, $drawing->getWidth());
+        self::assertSame(200, $drawing->getHeight());
+        $drawing->setResizeProportional(true);
+        $drawing->setWidthAndHeight(300, 250);
+        // width increase% more than height, so scale width
+        self::assertSame(188, $drawing->getWidth());
+        self::assertSame(250, $drawing->getHeight());
+        $drawing->setResizeProportional(false);
+        $drawing->setWidthAndHeight(150, 200);
+        $drawing->setResizeProportional(true);
+        // height increase% more than width, so scale height
+        $drawing->setWidthAndHeight(175, 350);
+        self::assertSame(175, $drawing->getWidth());
+        self::assertSame(234, $drawing->getHeight());
     }
 }
