093ce73b2974cdc746693ff6e953a1bcbfb809f3
oleibman <10341515+oleibman@users.noreply.github.com>
2022-03-17 19:07:43 -0700

    Resolve Scrutinizer Reports for Samples
    These are handled about 50-50 between code changes when reasonable, and annotations when not. No source code is changed.

--- samples/Reader/21_Reader_CSV_Long_Integers_with_String_Value_Binder.php
+++ samples/Reader/21_Reader_CSV_Long_Integers_with_String_Value_Binder.php
@@ -12,10 +12,10 @@ $inputFileType = 'Csv';
 $inputFileName = __DIR__ . '/sampleData/longIntegers.csv';
 
 $reader = IOFactory::createReader($inputFileType);
-$helper->log('Loading file ' . pathinfo($inputFileName, PATHINFO_BASENAME) . ' into WorkSheet #1 using IOFactory with a defined reader type of ' . $inputFileType);
+$helper->log('Loading file ' . /** @scrutinizer ignore-type */ pathinfo($inputFileName, PATHINFO_BASENAME) . ' into WorkSheet #1 using IOFactory with a defined reader type of ' . $inputFileType);
 
 $spreadsheet = $reader->load($inputFileName);
-$spreadsheet->getActiveSheet()->setTitle(pathinfo($inputFileName, PATHINFO_BASENAME));
+$spreadsheet->getActiveSheet()->setTitle(/** @scrutinizer ignore-type */ pathinfo($inputFileName, PATHINFO_BASENAME));
 
 $helper->log($spreadsheet->getSheetCount() . ' worksheet' . (($spreadsheet->getSheetCount() == 1) ? '' : 's') . ' loaded');
 $loadedSheetNames = $spreadsheet->getSheetNames();
