d9118391ac14471348cef69c90a8ff99af572164
oleibman <10341515+oleibman@users.noreply.github.com>
2022-04-10 22:08:32 -0700

    Update RichTextSizeTest.php
    Eliminate Phpstan failure.

--- tests/PhpSpreadsheetTests/Reader/Xls/RichTextSizeTest.php
+++ tests/PhpSpreadsheetTests/Reader/Xls/RichTextSizeTest.php
@@ -8,23 +8,16 @@ use PhpOffice\PhpSpreadsheetTests\Functional\AbstractFunctional;
 
 class RichTextSizeTest extends AbstractFunctional
 {
-    /**
-     * @var Spreadsheet
-     */
-    private $spreadsheet;
-
-    protected function setup(): void
+    public function testRichTextRunSize(): void
     {
         $filename = 'tests/data/Reader/XLS/RichTextFontSize.xls';
         $reader = new Xls();
-        $this->spreadsheet = $reader->load($filename);
-    }
-
-    public function testRichTextRunSize(): void
-    {
-        $sheet = $this->spreadsheet->getSheetByName('橱柜门板');
+        $spreadsheet = $reader->load($filename);
+        $sheet = $spreadsheet->getSheetByName('橱柜门板');
+        self::assertNotNull($sheet);
         $text = $sheet->getCell('L15')->getValue();
         $elements = $text->getRichTextElements();
         self::assertEquals(10, $elements[2]->getFont()->getSize());
+        $spreadsheet->disconnectWorksheets();
     }
 }
