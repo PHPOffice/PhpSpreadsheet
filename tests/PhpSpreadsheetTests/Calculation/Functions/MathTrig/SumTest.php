1bd741a7d0d8e68f6ffa9b28ac031bf5b6e1e08b
u01jmg3 <u01jmg3@gmail.com>
2022-07-28 10:46:57 +0100

    Provide test coverage for SUM combined with INDEX/MATCH

--- tests/PhpSpreadsheetTests/Calculation/Functions/MathTrig/SumTest.php
+++ tests/PhpSpreadsheetTests/Calculation/Functions/MathTrig/SumTest.php
@@ -2,6 +2,8 @@
 
 namespace PhpOffice\PhpSpreadsheetTests\Calculation\Functions\MathTrig;
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+
 class SumTest extends AllSetupTeardown
 {
     /**
@@ -43,5 +45,28 @@ class SumTest extends AllSetupTeardown
     public function providerSUMLiterals(): array
     {
         return require 'tests/data/Calculation/MathTrig/SUMLITERALS.php';
+    }
+
+    public function testSumWithIndexMatch(): void
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet1 = $spreadsheet->getActiveSheet();
+        $sheet1->setTitle('Formula');
+        $sheet1->fromArray(
+            [
+                ['Number', 'Formula'],
+                [83, '=SUM(4 * INDEX(Lookup!B2, MATCH(A2, Lookup!A2, 0)))'],
+            ]
+        );
+        $sheet2 = $spreadsheet->createSheet();
+        $sheet2->setTitle('Lookup');
+        $sheet2->fromArray(
+            [
+                ['Lookup', 'Match'],
+                [83, 16],
+            ]
+        );
+        self::assertSame(64, $sheet1->getCell('B2')->getCalculatedValue());
+        $spreadsheet->disconnectWorksheets();
     }
 }
