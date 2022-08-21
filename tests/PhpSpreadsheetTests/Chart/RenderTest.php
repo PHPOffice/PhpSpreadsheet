d55978cf931ced2bc9f0d982bb6b8269fbace1f9
oleibman <10341515+oleibman@users.noreply.github.com>
2022-08-20 19:58:43 -0700

    Correct Namespaces in 11 Tests (#3020)
    The setup for unit testing in Github in the "Install dependencies" log reports 11 members as "does not comply with with psr-4 autoloading standard." In each case, it is because the test namespace does not match the directory; in most cases, it was caused by the member being moved from one directory to another without changing the namespace declaration. No harm results from these problems, but there's also no reason to not correct them.

--- tests/PhpSpreadsheetTests/Chart/RenderTest.php
+++ tests/PhpSpreadsheetTests/Chart/RenderTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PhpOffice\PhpSpreadsheetTests\Writer\Xlsx;
+namespace PhpOffice\PhpSpreadsheetTests\Chart;
 
 use PhpOffice\PhpSpreadsheet\Chart\Chart;
 use PHPUnit\Framework\TestCase;
