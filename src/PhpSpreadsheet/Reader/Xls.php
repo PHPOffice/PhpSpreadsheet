26e17277e150e56b4922980e151a6b1f4345673d
oleibman <10341515+oleibman@users.noreply.github.com>
2022-11-21 15:31:13 -0800

    Eliminate Some Scrutinizer 'Major' Problems Part 5 (#3161)
    * Eliminate Some Scrutinizer 'Major' Problems Part 5
    
    More of the same. 2 or 3 of these still to go after this.
    
    Note one deprecation. Shared/Drawing has a method imagecreatefrombmp. However, that was introduced as a native PHP function with 7.2, so the method is no longer needed.
    
    * More Silliness
    
    Keep trying.
    
    * If At First You Don't Succeed
    
    Try, try again.

--- src/PhpSpreadsheet/Reader/Xls.php
+++ src/PhpSpreadsheet/Reader/Xls.php
@@ -4947,8 +4947,6 @@ class Xls extends BaseReader
                 case 0x28:
                     // TODO: Investigate structure for .xls SHEETLAYOUT record as saved by MS Office Excel 2007
                     return;
-
-                    break;
             }
         }
     }
