56950a43bb2a1c3f73e5f59b8cf1e8ebad1a7520
Roberts Sukonovs <roberts.sukonovs@gmail.com>
2023-01-06 10:38:53 +0200

    Improve doc.

--- src/PhpSpreadsheet/Calculation/Kasko.php
+++ src/PhpSpreadsheet/Calculation/Kasko.php
@@ -9,6 +9,15 @@ class Kasko
      * Problem described here: https://github.com/PHPOffice/PhpSpreadsheet/issues/1789
      * It was fixed by just adding validation, but our templates are full of formulas like:
      * =ROUND('1.111'; 0)
+     *
+     * Other possible functions that we are not using, but might break:
+     *
+     * MathTrig:
+     * ABS, ACOS, ACOSH, ASIN, ASINH, ATAN, ATANH,
+     * COS, COSH, DEGREES (rad2deg), EXP, LN (log), LOG10,
+     * RADIANS (deg2rad), REPT (str_repeat), SIN, SINH, SQRT, TAN, TANH.
+     *
+     * TextData function (REPT) is also affected.
      */
     public static function ROUND($num, $precision)
     {
