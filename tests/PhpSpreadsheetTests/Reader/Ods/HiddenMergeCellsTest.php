f17270ed614a5939e2c6657b50533422c73982d2
oleibman <10341515+oleibman@users.noreply.github.com>
2023-01-30 10:31:52 -0800

    Update HiddenMergeCellsTest.php

--- tests/PhpSpreadsheetTests/Reader/Ods/HiddenMergeCellsTest.php
+++ tests/PhpSpreadsheetTests/Reader/Ods/HiddenMergeCellsTest.php
@@ -7,7 +7,6 @@ use PHPUnit\Framework\TestCase;
 
 class HiddenMergeCellsTest extends TestCase
 {
-
     private const FILENAME = 'tests/data/Reader/Ods/HiddenMergeCellsTest.ods';
 
     public function testHiddenMergeCells(): void
