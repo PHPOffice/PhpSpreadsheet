b07c8c27c3e08c72a8678a1aa27edd335077f7d8
oleibman <10341515+oleibman@users.noreply.github.com>
2023-02-13 18:15:30 -0800

    Resolve Phpstan Messages - Statistical/Trend and Shared/Trend
    Reduce number of Phpstan messages by addressing their issues. The changes in this PR are all to doc blocks; no executable code is changed.

--- src/PhpSpreadsheet/Calculation/Statistical/Trends.php
+++ src/PhpSpreadsheet/Calculation/Statistical/Trends.php
@@ -21,6 +21,10 @@ class Trends
         }
     }
 
+    /**
+     * @param array $array1
+     * @param array $array2
+     */
     private static function checkTrendArrays(&$array1, &$array2): void
     {
         if (!is_array($array1)) {
@@ -168,7 +172,7 @@ class Trends
             $returnArray[0][] = [$bestFitExponential->getValueOfYForX($xValue)];
         }
 
-        return $returnArray;
+        return $returnArray; //* @phpstan-ignore-line
     }
 
     /**
@@ -425,6 +429,6 @@ class Trends
             $returnArray[0][] = [$bestFitLinear->getValueOfYForX($xValue)];
         }
 
-        return $returnArray;
+        return $returnArray; //* @phpstan-ignore-line
     }
 }
