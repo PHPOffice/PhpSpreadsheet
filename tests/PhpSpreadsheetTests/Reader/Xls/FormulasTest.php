fc6174d726b7063ad819292ce72a223b21f29057
oleibman <10341515+oleibman@users.noreply.github.com>
2023-02-20 05:27:26 -0800

    Complete Functions Coverage for Xls (except GETPIVOTDATA)
    Finish the work started in PR #3371 and PR #3340.

--- tests/PhpSpreadsheetTests/Reader/Xls/FormulasTest.php
+++ tests/PhpSpreadsheetTests/Reader/Xls/FormulasTest.php
@@ -40,4 +40,21 @@ class FormulasTest extends AbstractFunctional
         self::assertSame($originalArray, $newArray);
         $newSpreadsheet->disconnectWorksheets();
     }
+
+    public function testOtherFormulas(): void
+    {
+        // This file was created with Excel 2003.
+        $filename = 'tests/data/Reader/XLS/formulas.other.xls';
+        $reader = new Xls();
+        $spreadsheet = $reader->load($filename);
+        $sheet = $spreadsheet->getActiveSheet();
+        $originalArray = $sheet->toArray(null, false, false, false);
+
+        $newSpreadsheet = $this->writeAndReload($spreadsheet, 'Xls');
+        $spreadsheet->disconnectWorksheets();
+        $newWorksheet = $newSpreadsheet->getActiveSheet();
+        $newArray = $newWorksheet->toArray(null, false, false, false);
+        self::assertSame($originalArray, $newArray);
+        $newSpreadsheet->disconnectWorksheets();
+    }
 }
