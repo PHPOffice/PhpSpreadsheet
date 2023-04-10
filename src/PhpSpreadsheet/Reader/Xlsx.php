80921f679d04821dc26e7173b376606b44276ec4
riverdale74 <tapdsp@gmail.com>
2023-04-10 14:14:50 -0400

    Update Xlsx.php
    Handle "default" cases by creating an empty string table entry.
    
    I added these lines after observing that $sharedStrings was being indexed incorrectly with a test xlsx file. Adding these lines fixes that error.

--- src/PhpSpreadsheet/Reader/Xlsx.php
+++ src/PhpSpreadsheet/Reader/Xlsx.php
@@ -726,6 +726,8 @@ class Xlsx extends BaseReader
                                     $sharedStrings[] = StringHelper::controlCharacterOOXML2PHP((string) $val->t);
                                 } elseif (isset($val->r)) {
                                     $sharedStrings[] = $this->parseRichText($val);
+                                } else {
+                                    $sharedStrings[] = '';
                                 }
                             }
                         }
