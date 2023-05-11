9a68d59da9f208f83eb5bbb244aa29d0e4efec4e
dubox <dubo_x@qq.com>
2023-05-11 06:06:04 +0000

    Fix code styles

--- tests/PhpSpreadsheetTests/Writer/Html/MemoryDrawingOffsetTest.php
+++ tests/PhpSpreadsheetTests/Writer/Html/MemoryDrawingOffsetTest.php
@@ -11,32 +11,37 @@ class MemoryDrawingOffsetTest extends TestCase
 {
     /**
      * @dataProvider dataProvider
-     */ 
-    public function testMemoryDrawingOffset($w ,$h ,$x ,$y): void
+     */
+    public function testMemoryDrawingOffset(int $w, int $h, int $x, int $y): void
     {
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
         $drawing = new MemoryDrawing();
-        $drawing->setImageResource(\imagecreatefromstring(file_get_contents("https://avatars.githubusercontent.com/u/1836015"))) //->setPath($this->getPath())//  
+        if (($image = file_get_contents("https://avatars.githubusercontent.com/u/1836015")) === false)
+            throw new \Exception('image err1');
+        if (($image = imagecreatefromstring($image)) === false)
+            throw new \Exception('image err2');
+
+        $drawing->setImageResource($image)
             ->setResizeProportional(false) //是否保持比例
             ->setWidthAndHeight($w, $h) //图片宽高,原始尺寸 100*100
             ->setOffsetX($x)
             ->setOffsetY($y)
             ->setWorksheet($sheet);
-        
+
         $writer = new Html($spreadsheet);
         $html = $writer->generateHtmlAll();
-        self::assertStringContainsString('width:' . $w . 'px;left: ' .$x . 'px; top: ' . $y . 'px;position: absolute;', $html);
+        self::assertStringContainsString('width:' . $w . 'px;left: ' . $x . 'px; top: ' . $y . 'px;position: absolute;', $html);
         $spreadsheet->disconnectWorksheets();
         unset($spreadsheet);
     }
 
-    public function dataProvider()
+    public function dataProvider(): array
     {
         return [
-            [33, 19, 0 ,20],
-            [129, 110, 12,-3],
-            [55, 110, 33,42],
+            [33, 19, 0, 20],
+            [129, 110, 12, -3],
+            [55, 110, 33, 42],
         ];
     }
 }
