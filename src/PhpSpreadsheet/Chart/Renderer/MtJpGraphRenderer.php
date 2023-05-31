b3f9ececabe8fc236c45f72b65296b1338d66161
Mikhail Oleynik <mishutka@mito-team.com>
2023-05-31 17:37:21 +0300

    Loading mitoteam/jpgraph 10.3+ in Extended Mode (with some bugs fixed)

--- src/PhpSpreadsheet/Chart/Renderer/MtJpGraphRenderer.php
+++ src/PhpSpreadsheet/Chart/Renderer/MtJpGraphRenderer.php
@@ -3,12 +3,12 @@
 namespace PhpOffice\PhpSpreadsheet\Chart\Renderer;
 
 /**
- * Jpgraph is not oficially maintained in Composer.
+ * Jpgraph is not officially maintained by Composer at packagist.org.
  *
  * This renderer implementation uses package
  * https://packagist.org/packages/mitoteam/jpgraph
  *
- * This package is up to date for August 2022 and has PHP 8.1 support.
+ * This package is up to date for June 2023 and has PHP 8.2 support.
  */
 class MtJpGraphRenderer extends JpGraphRendererBase
 {
@@ -19,17 +19,20 @@ class MtJpGraphRenderer extends JpGraphRendererBase
             return;
         }
 
-        \mitoteam\jpgraph\MtJpGraph::load([
-            'bar',
-            'contour',
-            'line',
-            'pie',
-            'pie3d',
-            'radar',
-            'regstat',
-            'scatter',
-            'stock',
-        ]);
+        \mitoteam\jpgraph\MtJpGraph::load(
+            array(
+                'bar',
+                'contour',
+                'line',
+                'pie',
+                'pie3d',
+                'radar',
+                'regstat',
+                'scatter',
+                'stock',
+            ),
+            true // enable Extended Mode
+        );
 
         $loaded = true;
     }
