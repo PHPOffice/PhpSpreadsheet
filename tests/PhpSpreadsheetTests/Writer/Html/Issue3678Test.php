PHPOffice/PhpSpreadsheet

commit e97428ba6708615fd12b411c9ef2a079bcb3d252

Html Writer - Do Not Generate background-color When Fill is None (#3016)
* Html Writer - Do Not Generate background-color When Fill is None

For PR #3002, I noted that there was a problem with Dompdf truncating images. I raised an issue with them (https://github.com/dompdf/dompdf/issues/2980), and they agree that there is a bug; however, they also suggested a workaround, namely omitting background-color from any cells which the image overlays. That did not at first appear to be a solution which could be generalized for PhpSpreasheet. However, investigating further, I saw that Html Writer is generating background-color for all cells, even though most of them use the default Fill type None (which suggests that background-color should not be specified after all). So this PR changes HTML Writer to generate background-color only when the user has actually set Fill type to something other than None. This is not a complete workaround for the Dompdf problem - we will still see truncation if the image overlays a cell which does specify a Fill type - however, it is almost certainly good enough for most use cases.

In addition to that change, I made the generated Html a little smaller and the code a little more efficient by combining the TD and TH styles for each cell into a single declaration and calling createCssStyle only once.

* Revamp One Test

Look for both td.style and th.style instead of just td.style in test.

src/PhpSpreadsheet/Writer/Html.php
@@ -940,8 +940,8 @@ class Html extends BaseWriter
 
         // Calculate cell style hashes
         foreach ($this->spreadsheet->getCellXfCollection() as $index => $style) {
-            $css['td.style' . $index] = $this->createCSSStyle($style);
-            $css['th.style' . $index] = $this->createCSSStyle($style);
+            $css['td.style' . $index . ', th.style' . $index] = $this->createCSSStyle($style);
+            //$css['th.style' . $index] = $this->createCSSStyle($style);
         }
 
         // Fetch sheets
@@ -1094,9 +1094,10 @@ class Html extends BaseWriter
         $css = [];
 
         // Create CSS
-        $value = $fill->getFillType() == Fill::FILL_NONE ?
-            'white' : '#' . $fill->getStartColor()->getRGB();
-        $css['background-color'] = $value;
+        if ($fill->getFillType() !== Fill::FILL_NONE) {
+            $value = '#' . $fill->getStartColor()->getRGB();
+            $css['background-color'] = $value;
+        }
 
         return $css;
     }

tests/PhpSpreadsheetTests/Writer/Html/VisibilityTest.php
@@ -99,11 +99,11 @@ class VisibilityTest extends Functional\AbstractFunctional
         self::assertEquals(1, $rowsrch);
         $rowsrch = preg_match('/^\\s*table[.]sheet0 tr[.]row1 [{] height:25pt [}]\\s*$/m', $html);
         self::assertEquals(1, $rowsrch);
-        $rowsrch = preg_match('/^\\s*td[.]style1 [{].*text-decoration:line-through;.*[}]\\s*$/m', $html);
+        $rowsrch = preg_match('/^\\s*td[.]style1, th[.]style1 [{].*text-decoration:line-through;.*[}]\\s*$/m', $html);
         self::assertEquals(1, $rowsrch);
-        $rowsrch = preg_match('/^\\s*td[.]style2 [{].*text-decoration:underline line-through;.*[}]\\s*$/m', $html);
+        $rowsrch = preg_match('/^\\s*td[.]style2, th[.]style2 [{].*text-decoration:underline line-through;.*[}]\\s*$/m', $html);
         self::assertEquals(1, $rowsrch);
-        $rowsrch = preg_match('/^\\s*td[.]style3 [{].*text-decoration:underline;.*[}]\\s*$/m', $html);
+        $rowsrch = preg_match('/^\\s*td[.]style3, th[.]style3 [{].*text-decoration:underline;.*[}]\\s*$/m', $html);
         self::assertEquals(1, $rowsrch);
 
         $this->writeAndReload($spreadsheet, 'Html');

commit f7069b55c637694a38e440e8a3b4e02d71a656fb

Html Writer Styles when Using Inline Css
Fix #3678. Problem introduced by PR #3016. Combining `td` and `th` styles into a single declaration greatly reduces file size when `useInlineCss` is false, which is the default. However, generating code with the non-default option was not changed to use the combined declaration, so styling was lost. This PR rectifies that error.

src/PhpSpreadsheet/Writer/Html.php
@@ -1384,14 +1384,10 @@ class Html extends BaseWriter
                 $cssClass .= ' style' . $cell->getXfIndex();
                 $cssClass .= ' ' . $cell->getDataType();
             } elseif (is_array($cssClass)) {
-                if ($cellType == 'th') {
-                    if (isset($this->cssStyles['th.style' . $cell->getXfIndex()])) {
-                        $cssClass = array_merge($cssClass, $this->cssStyles['th.style' . $cell->getXfIndex()]);
-                    }
-                } else {
-                    if (isset($this->cssStyles['td.style' . $cell->getXfIndex()])) {
-                        $cssClass = array_merge($cssClass, $this->cssStyles['td.style' . $cell->getXfIndex()]);
-                    }
+                $index = $cell->getXfIndex();
+                $styleIndex = 'td.style' . $index . ', th.style' . $index;
+                if (isset($this->cssStyles[$styleIndex])) {
+                    $cssClass = array_merge($cssClass, $this->cssStyles[$styleIndex]);
                 }
 
                 // General horizontal alignment: Actual horizontal alignment depends on dataType

tests/PhpSpreadsheetTests/Writer/Html/Issue3678Test.php
@@ -1,0 +1,37 @@
+<?php
+
+namespace PhpOffice\PhpSpreadsheetTests\Writer\Html;
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Writer\Html;
+use PHPUnit\Framework\TestCase;
+
+class Issue3678Test extends TestCase
+{
+    public function testInlineAndNot(): void
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->getCell('A1')->setValue(1);
+        $styleArray = [
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'color' => ['rgb' => 'FFFF00'],
+            ],
+        ];
+        $sheet->getStyle('A1')->applyFromArray($styleArray);
+        $style1 = "vertical-align:bottom; border-bottom:none #000000; border-top:none #000000; border-left:none #000000; border-right:none #000000; color:#000000; font-family:'Calibri'; font-size:11pt; background-color:#FFFF00";
+        $style2 = $style1 . "; text-align:right; width:42pt";
+        $writer = new Html($spreadsheet);
+        $html = $writer->generateHtmlAll();
+        self::assertStringContainsString('td.style1, th.style1 { ' . $style1 . ' }', $html);
+        self::assertStringContainsString('<td class="column0 style1 n">1</td>', $html);
+        self::assertStringContainsString('table.sheet0 col.col0 { width:42pt }', $html);
+        self::assertStringContainsString('.n { text-align:right }', $html);
+        $writer->setUseInlineCss(true);
+        $html = $writer->generateHtmlAll();
+        self::assertStringContainsString('<td style="' . $style2 . '">1</td>', $html);
+        $spreadsheet->disconnectWorksheets();
+    }
+}