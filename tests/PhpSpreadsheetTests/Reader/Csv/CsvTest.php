93509c2c293e0b172965b60d11d34339b718663f
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-09-05 19:46:26 -0700

    Drop PHP 7.4 support
    This is according to our formal, published, policy to only support
    EOL PHP after 6 months.
    
    See https://phpspreadsheet.readthedocs.io/en/latest/#php-version-support
    
    Also share the exact same dev deps across all PHP version for GitHub
    Actions so runs are faster, much most importantly they are stable and
    predictable. And we decide manually when we want to migrate to PHPUnit
    10.
    
    Fixes #3634
    Closes #3710

--- tests/PhpSpreadsheetTests/Reader/Csv/CsvTest.php
+++ tests/PhpSpreadsheetTests/Reader/Csv/CsvTest.php
@@ -248,7 +248,7 @@ class CsvTest extends TestCase
         return [
             ['\\', ';'],
             ["\x0", ','],
-            [(version_compare(PHP_VERSION, '7.4') < 0) ? "\x0" : '', ','],
+            ['', ','],
         ];
     }
 
--- tests/PhpSpreadsheetTests/SettingsTest.php
+++ tests/PhpSpreadsheetTests/SettingsTest.php
@@ -8,26 +8,8 @@ use PHPUnit\Framework\TestCase;
 
 class SettingsTest extends TestCase
 {
-    /**
-     * @var bool
-     */
-    private $prevValue;
-
-    protected function setUp(): void
-    {
-        // php 8.+ deprecated libxml_disable_entity_loader() - It's on by default
-        if (\PHP_VERSION_ID < 80000) {
-            $this->prevValue = libxml_disable_entity_loader();
-            libxml_disable_entity_loader(false); // Enable entity loader
-        }
-    }
-
     protected function tearDown(): void
     {
-        // php 8.+ deprecated libxml_disable_entity_loader() - It's on by default
-        if (\PHP_VERSION_ID < 80000) {
-            libxml_disable_entity_loader($this->prevValue);
-        }
         Settings::setCache(null);
     }
 
@@ -35,10 +17,6 @@ class SettingsTest extends TestCase
     {
         $result = Settings::getLibXmlLoaderOptions();
         self::assertTrue((bool) ((LIBXML_DTDLOAD | LIBXML_DTDATTR) & $result));
-        // php 8.+ deprecated libxml_disable_entity_loader() - It's on by default
-        if (\PHP_VERSION_ID < 80000) {
-            self::assertFalse(libxml_disable_entity_loader());
-        }
     }
 
     public function testSetXMLSettings(): void
@@ -47,10 +25,6 @@ class SettingsTest extends TestCase
         Settings::setLibXmlLoaderOptions(LIBXML_DTDLOAD | LIBXML_DTDATTR | LIBXML_DTDVALID);
         $result = Settings::getLibXmlLoaderOptions();
         self::assertTrue((bool) ((LIBXML_DTDLOAD | LIBXML_DTDATTR | LIBXML_DTDVALID) & $result));
-        // php 8.+ deprecated libxml_disable_entity_loader() - It's on by default
-        if (\PHP_VERSION_ID < 80000) {
-            self::assertFalse(libxml_disable_entity_loader());
-        }
         Settings::setLibXmlLoaderOptions($original);
     }
 
