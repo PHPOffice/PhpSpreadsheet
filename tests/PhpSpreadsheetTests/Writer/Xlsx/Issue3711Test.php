71f96c4c5addc8b0a0d31a1a5b2b3849efdec28a
oleibman <10341515+oleibman@users.noreply.github.com>
2023-09-07 08:17:47 -0700

    Improve Test
    Better match to original issue.

--- tests/PhpSpreadsheetTests/Writer/Xlsx/Issue3711Test.php
+++ tests/PhpSpreadsheetTests/Writer/Xlsx/Issue3711Test.php
@@ -12,16 +12,21 @@ class Issue3711Test extends AbstractFunctional
 {
     public function testIssue3711(): void
     {
-        // Issue 3443 - float being used as index in StringTable.
+        // Issue 3711 - float being used as index in StringTable.
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
-        $sheet->getCell('A1')->setValue('21.8');
+        $sheet->getCell('A1')->setValue('21.5');
+        self::assertSame(21.5, $sheet->getCell('A1')->getValue();
         $sheet->getCell('A1')->setDataType(DataType::TYPE_STRING);
+        $sheet->getCell('A2')->setValue('21');
+        self::assertSame(21, $sheet->getCell('A2')->getValue();
+        $sheet->getCell('A2')->setDataType(DataType::TYPE_STRING);
 
         $reloadedSpreadsheet = $this->writeAndReload($spreadsheet, 'Xlsx');
         $spreadsheet->disconnectWorksheets();
         $rsheet = $reloadedSpreadsheet->getActiveSheet();
-        self::assertSame('21.8', $rsheet->getCell('A1')->getValue());
+        self::assertSame('21.5', $rsheet->getCell('A1')->getValue());
+        self::assertSame('21', $rsheet->getCell('A2')->getValue());
         $reloadedSpreadsheet->disconnectWorksheets();
     }
 }
