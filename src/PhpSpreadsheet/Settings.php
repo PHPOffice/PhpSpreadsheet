6160dc98a1ca8c646bce90ee2f3427616ab5d197
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-09-12 11:05:52 +0800

    phpcs

--- src/PhpSpreadsheet/Settings.php
+++ src/PhpSpreadsheet/Settings.php
@@ -2,7 +2,6 @@
 
 namespace PhpOffice\PhpSpreadsheet;
 
-use const ENT_COMPAT;
 use PhpOffice\PhpSpreadsheet\Calculation\Calculation;
 use PhpOffice\PhpSpreadsheet\Chart\Renderer\IRenderer;
 use PhpOffice\PhpSpreadsheet\Collection\Memory;
