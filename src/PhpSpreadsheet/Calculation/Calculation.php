db35a4127d8d3e1d1161561359239993ce7038bd
oleibman <10341515+oleibman@users.noreply.github.com>
2023-09-30 13:16:06 -0700

    Upgrades in Advance of Dependabot (#3757)
    More convenient to handle it today than tomorrow. Phpstan flags 1 new statement, but it is incorrect - annotate it.

--- src/PhpSpreadsheet/Calculation/Calculation.php
+++ src/PhpSpreadsheet/Calculation/Calculation.php
@@ -4409,7 +4409,7 @@ class Calculation
                             [$rangeWS2, $val] = Worksheet::extractSheetTitle($val, true);
                             if ($rangeWS2 !== '') {
                                 $rangeWS2 .= '!';
-                            } else {
+                            } else { // @phpstan-ignore-line
                                 $rangeWS2 = $rangeWS1;
                             }
 
