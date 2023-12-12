af5b6316c9b17163b33f751e90d4fc5fc1a1beb2
oleibman <10341515+oleibman@users.noreply.github.com>
2023-12-12 13:28:26 -0800

    Cover A Bit More Code

--- tests/PhpSpreadsheetTests/Chart/ChartCloneTest.php
+++ tests/PhpSpreadsheetTests/Chart/ChartCloneTest.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace PhpOffice\PhpSpreadsheetTests\Chart;
 
+use PhpOffice\PhpSpreadsheet\Chart\Chart;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeries;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeriesValues;
+use PhpOffice\PhpSpreadsheet\Chart\Layout;
+use PhpOffice\PhpSpreadsheet\Chart\Legend as ChartLegend;
+use PhpOffice\PhpSpreadsheet\Chart\PlotArea;
 use PhpOffice\PhpSpreadsheet\Chart\Properties;
+use PhpOffice\PhpSpreadsheet\Chart\Title;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx as XlsxReader;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheetTests\Functional\AbstractFunctional;
 
-/**
- * Confirm cloning worksheet works as expected.
- * This class tests everything except:
- *   DataSeriesValues - no coverage for fillColor as array.
- *   Title - no coverage for caption when not array.
- */
 class ChartCloneTest extends AbstractFunctional
 {
     private const DIRECTORY = 'samples' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
@@ -158,6 +160,127 @@ class ChartCloneTest extends AbstractFunctional
         $plotLabels = $plotGroup[0]->getPlotLabels();
         self::assertCount(1, $plotLabels);
         self::assertCount(3, $plotLabels[0]->getTrendLines());
+
+        $spreadsheet->disconnectWorksheets();
+    }
+
+    public function testCloneFillColorArray(): void
+    {
+        // code borrowed from BarChartCustomColorsTest
+        $spreadsheet = new Spreadsheet();
+        $worksheet = $spreadsheet->getActiveSheet();
+        $worksheet->fromArray(
+            [
+                ['', 2010, 2011, 2012],
+                ['Q1', 12, 15, 21],
+                ['Q2', 56, 73, 86],
+                ['Q3', 52, 61, 69],
+                ['Q4', 30, 32, 0],
+            ]
+        );
+        // Custom colors for dataSeries (gray, blue, red, orange)
+        $colors = [
+            'cccccc',
+            '*accent1', // use schemeClr, was '00abb8',
+            '/green', // use prstClr, was 'b8292f',
+            'eb8500',
+        ];
+
+        // Set the Labels for each data series we want to plot
+        //     Datatype
+        //     Cell reference for data
+        //     Format Code
+        //     Number of datapoints in series
+        //     Data values
+        //     Data Marker
+        $dataSeriesLabels1 = [
+            new DataSeriesValues(
+                DataSeriesValues::DATASERIES_TYPE_STRING,
+                'Worksheet!$C$1',
+                null,
+                1
+            ), // 2011
+        ];
+        // Set the X-Axis Labels
+        //     Datatype
+        //     Cell reference for data
+        //     Format Code
+        //     Number of datapoints in series
+        //     Data values
+        //     Data Marker
+        $xAxisTickValues1 = [
+            new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Worksheet!$A$2:$A$5', null, 4), // Q1 to Q4
+        ];
+        // Set the Data values for each data series we want to plot
+        //     Datatype
+        //     Cell reference for data
+        //     Format Code
+        //     Number of datapoints in series
+        //     Data values
+        //     Data Marker
+        //     Custom Colors
+        $dataSeriesValues1Element = new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Worksheet!$C$2:$C$5', null, 4);
+        $dataSeriesValues1Element->setFillColor($colors);
+        $dataSeriesValues1 = [$dataSeriesValues1Element];
+
+        // Build the dataseries
+        $series1 = new DataSeries(
+            DataSeries::TYPE_PIECHART, // plotType
+            null, // plotGrouping (Pie charts don't have any grouping)
+            range(0, count($dataSeriesValues1) - 1), // plotOrder
+            $dataSeriesLabels1, // plotLabel
+            $xAxisTickValues1, // plotCategory
+            $dataSeriesValues1          // plotValues
+        );
+
+        // Set up a layout object for the Pie chart
+        $layout1 = new Layout();
+        $layout1->setShowVal(true);
+        $layout1->setShowPercent(true);
+
+        // Set the series in the plot area
+        $plotArea1 = new PlotArea($layout1, [$series1]);
+        // Set the chart legend
+        $legend1 = new ChartLegend(ChartLegend::POSITION_RIGHT, null, false);
+
+        $title1 = new Title('Test Pie Chart');
+
+        // Create the chart
+        $chart1 = new Chart(
+            'chart1', // name
+            $title1, // title
+            $legend1, // legend
+            $plotArea1, // plotArea
+            true, // plotVisibleOnly
+            DataSeries::EMPTY_AS_GAP, // displayBlanksAs
+            null, // xAxisLabel
+            null  // no Y-Axis for Pie Chart
+        );
+
+        // Set the position where the chart should appear in the worksheet
+        $chart1->setTopLeftPosition('A7');
+        $chart1->setBottomRightPosition('H20');
+
+        // Add the chart to the worksheet
+        $worksheet->addChart($chart1);
+
+        $sheet = clone $worksheet;
+        $sheet->setTitle('test2');
+        $spreadsheet->addsheet($sheet);
+
+        $charts2 = $sheet->getChartCollection();
+        self::assertCount(1, $charts2);
+        $chart2 = $charts2[0];
+        self::assertNotNull($chart2);
+        self::assertSame('Test Pie Chart', $chart2->getTitle()?->getCaption());
+        $plotArea2 = $chart2->getPlotArea();
+        self::assertNotNull($plotArea2);
+        $dataSeries2 = $plotArea2->getPlotGroup();
+        self::assertCount(1, $dataSeries2);
+        $plotValues = $dataSeries2[0]->getPlotValues();
+        self::assertCount(1, $plotValues);
+        $fillColors = $plotValues[0]->getFillColor();
+        self::assertSame($colors, $fillColors);
 
         $spreadsheet->disconnectWorksheets();
     }
