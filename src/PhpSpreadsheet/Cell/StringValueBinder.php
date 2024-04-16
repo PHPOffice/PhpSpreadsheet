fdc3af74c9a621b4214876718f2ba5b71bf90741
mcorridor <corridor.marlo@gmail.com>
2024-04-16 13:15:48 +0800

    Update StringValueBinder.php
    Fix check for Formula data type because expression is intended to check if `convertFormula` is set to `true`.

--- src/PhpSpreadsheet/Cell/StringValueBinder.php
+++ src/PhpSpreadsheet/Cell/StringValueBinder.php
@@ -87,7 +87,7 @@ class StringValueBinder implements IValueBinder
             $cell->setValueExplicit($value, DataType::TYPE_BOOL);
         } elseif ((is_int($value) || is_float($value)) && $this->convertNumeric === false) {
             $cell->setValueExplicit($value, DataType::TYPE_NUMERIC);
-        } elseif (is_string($value) && strlen($value) > 1 && $value[0] === '=' && $this->convertFormula === false) {
+        } elseif (is_string($value) && strlen($value) > 1 && $value[0] === '=' && $this->convertFormula === true) {
             $cell->setValueExplicit($value, DataType::TYPE_FORMULA);
         } else {
             if (is_string($value) && strlen($value) > 1 && $value[0] === '=') {
