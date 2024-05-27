8c50287564d342ac6000d98dd2c56cd86cc108d1
timouche <timeo971@gmail.com>
2024-05-27 13:32:17 +0200

    test add a custom function call llm try 4

--- src/PhpSpreadsheet/Calculation/External/Llm.php
+++ src/PhpSpreadsheet/Calculation/External/Llm.php
@@ -11,7 +11,7 @@ class Llm
 
     
   
-    public static function call( string $prompt , string $model = "dolphinLama2.3",): string
+    public static function call(string $prompt,string $model="dolphinLama2.3"): string
     {
         return "falue";
     }
