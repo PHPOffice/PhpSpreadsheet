5d12967d092e6b454ce2b9a92f8e15f04d7f5504
timouche <timeo971@gmail.com>
2024-05-27 14:44:18 +0200

    test add a custom function call llm try 6

--- src/PhpSpreadsheet/Calculation/External/Llm.php
+++ src/PhpSpreadsheet/Calculation/External/Llm.php
@@ -2,15 +2,9 @@
 
 namespace PhpOffice\PhpSpreadsheet\Calculation\External;
 
-use PhpOffice\PhpSpreadsheet\Calculation\ArrayEnabled;
-use PhpOffice\PhpSpreadsheet\Calculation\Exception;
-
 class Llm
 {
-    use ArrayEnabled;
 
-    
-  
     public static function call(string $prompt, string $model = "dolphinLama2.3"): string
     {
         return "falue";
