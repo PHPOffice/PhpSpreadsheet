0ddaff3a21bbf829d02629940084028d7486780f
oleibman <10341515+oleibman@users.noreply.github.com>
2024-08-01 11:59:28 -0700

    Update Workbook.php

--- src/PhpSpreadsheet/Writer/Xlsx/Workbook.php
+++ src/PhpSpreadsheet/Writer/Xlsx/Workbook.php
@@ -157,7 +157,7 @@ class Workbook extends WriterPart
         //     because the file has changed
         $objWriter->writeAttribute('calcId', '999999');
         $objWriter->writeAttribute('calcMode', 'auto');
-        //    fullCalcOnLoad isn't needed if we arve calculating for the save
+        //    fullCalcOnLoad isn't needed if we will calculate before writing
         $objWriter->writeAttribute('calcCompleted', ($preCalculateFormulas) ? '1' : '0');
         $objWriter->writeAttribute('fullCalcOnLoad', ($preCalculateFormulas) ? '0' : '1');
         $objWriter->writeAttribute('forceFullCalc', ($preCalculateFormulas) ? '0' : '1');
