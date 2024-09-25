60b115c312b47c6a156ca5c5912c103c828d9f7e
oleibman <10341515+oleibman@users.noreply.github.com>
2024-09-24 17:55:03 -0700

    Forgot to Delete Some Lines

--- tests/PhpSpreadsheetTests/Reader/Xlsx/URLImageTest.php
+++ tests/PhpSpreadsheetTests/Reader/Xlsx/URLImageTest.php
@@ -30,16 +30,6 @@ class URLImageTest extends TestCase
             // Check if the source is a URL or a file path
             self::assertTrue($drawing->getIsURL());
             self::assertSame('https://phpspreadsheet.readthedocs.io/en/latest/topics/images/01-03-filter-icon-1.png', $drawing->getPath());
-            $imageContents = file_get_contents($drawing->getPath());
-            self::assertNotFalse($imageContents);
-            $filePath = tempnam(sys_get_temp_dir(), 'Drawing');
-            self::assertNotFalse($filePath);
-            self::assertNotFalse(file_put_contents($filePath, $imageContents));
-            $mimeType = mime_content_type($filePath);
-            unlink($filePath);
-            self::assertNotFalse($mimeType);
-            $extension = File::mime2ext($mimeType);
-            self::assertSame('png', $extension);
             self::assertSame(IMAGETYPE_PNG, $drawing->getType());
             self::assertSame(84, $drawing->getWidth());
             self::assertSame(44, $drawing->getHeight());
