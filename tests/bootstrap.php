4545ecf64844c7ac4d615f2da2503b0d5bbce8dd
oleibman <10341515+oleibman@users.noreply.github.com>
2024-11-12 19:23:20 -0800

    Downgrade Symfony/Process
    A bit messier than expected. Current version of symfony/process (used only in dev) has a security advisory. There is no version that can be upgraded to which supports Php8.0. However, a downgrade to V5 will work.
    
    When running with Php8.4, PhpUnit references E_STRICT, which is deprecated in 8.4. I don't understand why this doesn't happen with release1291. Be that as it may, we can't upgrade Phpunit to V10 because we need to support Php8.0. So, monkey around with tests/bootstrap.php to avoid problems.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,13 +25,31 @@ function phpunit10ErrorHandler(int $errno, string $errstr, string $filename, int
             return true; // message suppressed - stop error handling
         }
 
-        throw new Exception("$errstr $filename $lineno");
+        // This code applies only when running release210 with Php8.4.
+        // I don't get it at all. I think mitoteam is the victim of circumstance.
+        // We need to run PhpUnit 9 because we need to support Php8.0.
+        // We are at the highest release of PhpUnit9,
+        // but it refers to E_STRICT,
+        // which is deprecated in 8.4.
+        if (
+            str_contains($errstr, 'Constant ')
+            && str_contains($errstr, ' already defined')
+            && str_contains($filename, 'mitoteam')
+        ) {
+            return true;
+        }
+
+        if (!method_exists(PHPUnit\Framework\TestCase::class, 'setOutputCallback')) {
+            throw new Exception("$errstr $filename $lineno");
+        }
+
+        throw new Exception("$errstr");
     }
 
     return false; // continue error handling
 }
 
-if (!method_exists(PHPUnit\Framework\TestCase::class, 'setOutputCallback')) {
+if (!method_exists(PHPUnit\Framework\TestCase::class, 'setOutputCallback') || PHP_VERSION_ID >= 80400) {
     ini_set('error_reporting', (string) E_ALL);
     set_error_handler('phpunit10ErrorHandler');
 }
