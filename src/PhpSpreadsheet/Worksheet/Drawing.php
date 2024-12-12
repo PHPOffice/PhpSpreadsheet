83bd1f2b6bd6720383e6e39dbcfb3e4818c00293
oleibman <10341515+oleibman@users.noreply.github.com>
2024-12-11 22:23:31 -0800

    Update Drawing.php
    Put Connection and Accept under header

--- src/PhpSpreadsheet/Worksheet/Drawing.php
+++ src/PhpSpreadsheet/Worksheet/Drawing.php
@@ -117,8 +117,10 @@ class Drawing extends BaseDrawing
                     'ssl' => ['crypto_method' => STREAM_CRYPTO_METHOD_TLSv1_3_CLIENT],
                     'http' => [
                         'user_agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-                        'Connection: keep-alive',
-                        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                        'header' => [
+                            'Connection: keep-alive',
+                            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                        ],
                     ],
                 ]);
             }
