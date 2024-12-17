cf5bf089045499c1596e4357ae642f265072dca8
oleibman <10341515+oleibman@users.noreply.github.com>
2024-12-16 23:57:51 -0800

    Xlsx Reader Shared Formula with Boolean Results
    A solution, at least in part, for issue #4280. Xlsx Reader is not handling shared formulae correctly. As a result, some cells are treated as if they contain boolean values rather than formulae.

--- src/PhpSpreadsheet/Reader/Xlsx.php
+++ src/PhpSpreadsheet/Reader/Xlsx.php
@@ -854,6 +854,8 @@ class Xlsx extends BaseReader
                                         }
 
                                         // Read cell!
+                                        $useFormula = isset($c->f)
+                                            && ((string) $c->f !== '' || (isset($c->f->attributes()['t']) && strtolower((string) $c->f->attributes()['t']) === 'shared'));
                                         switch ($cellDataType) {
                                             case DataType::TYPE_STRING:
                                                 if ((string) $c->v != '') {
@@ -868,7 +870,7 @@ class Xlsx extends BaseReader
 
                                                 break;
                                             case DataType::TYPE_BOOL:
-                                                if (!isset($c->f) || ((string) $c->f) === '') {
+                                                if (!$useFormula) {
                                                     if (isset($c->v)) {
                                                         $value = self::castToBoolean($c);
                                                     } else {
@@ -883,16 +885,16 @@ class Xlsx extends BaseReader
 
                                                 break;
                                             case DataType::TYPE_STRING2:
-                                                if (isset($c->f)) {
+                                                if ($useFormula) {
                                                     $this->castToFormula($c, $r, $cellDataType, $value, $calculatedValue, 'castToString');
                                                     self::storeFormulaAttributes($c->f, $docSheet, $r);
                                                 } else {
-                                                     $value = self::castToString($c);
+                                                    $value = self::castToString($c);
                                                 }
 
                                                 break;
                                             case DataType::TYPE_INLINE:
-                                                if (isset($c->f)) {
+                                                if ($useFormula) {
                                                     $this->castToFormula($c, $r, $cellDataType, $value, $calculatedValue, 'castToError');
                                                     self::storeFormulaAttributes($c->f, $docSheet, $r);
                                                 } else {
@@ -901,7 +903,7 @@ class Xlsx extends BaseReader
 
                                                 break;
                                             case DataType::TYPE_ERROR:
-                                                if (!isset($c->f)) {
+                                                if (!$useFormula) {
                                                     $value = self::castToError($c);
                                                 } else {
                                                     // Formula
@@ -916,7 +918,7 @@ class Xlsx extends BaseReader
 
                                                 break;
                                             default:
-                                                if (!isset($c->f)) {
+                                                if (!$useFormula) {
                                                     $value = self::castToString($c);
                                                     if (is_numeric($value)) {
                                                         $value += 0;
