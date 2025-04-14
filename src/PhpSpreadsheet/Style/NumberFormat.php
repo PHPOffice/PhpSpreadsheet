2bb661345265cb3d6cc07644537c3bac24d6cd7e
John Crisp <jcrisp@safeandsoundit.co.uk>
2025-04-14 10:54:37 +0200

    Update NumberFormat.php
    Add format number with thousands separator and no decimal "#,##0'"

--- src/PhpSpreadsheet/Style/NumberFormat.php
+++ src/PhpSpreadsheet/Style/NumberFormat.php
@@ -16,6 +16,7 @@ class NumberFormat extends Supervisor
     const FORMAT_NUMBER_00 = '0.00';
     const FORMAT_NUMBER_COMMA_SEPARATED1 = '#,##0.00';
     const FORMAT_NUMBER_COMMA_SEPARATED2 = '#,##0.00_-';
+    const FORMAT_NUMBER_COMMA_SEPARATED3 = '#,##0';
 
     const FORMAT_PERCENTAGE = '0%';
     const FORMAT_PERCENTAGE_0 = '0.0%';
