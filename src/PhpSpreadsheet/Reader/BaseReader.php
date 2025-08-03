334a67797ace574d1d37c0992ffe283b7415471a
oleibman <10341515+oleibman@users.noreply.github.com>
2025-08-02 18:38:33 -0700

    Breaking Change When Loading External Images
    Images will be loaded from an external source (e.g. http://example.com/img.png) only if the reader is explicitly set to allow it via `$reader->setAllowExternalImages(true)`.

--- src/PhpSpreadsheet/Reader/BaseReader.php
+++ src/PhpSpreadsheet/Reader/BaseReader.php
@@ -51,7 +51,7 @@ abstract class BaseReader implements IReader
      * Improper specification of these within a spreadsheet
      * can subject the caller to security exploits.
      */
-    protected bool $allowExternalImages = true;
+    protected bool $allowExternalImages = false;
 
     /**
      * IReadFilter instance.
