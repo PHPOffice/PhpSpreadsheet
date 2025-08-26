83db0477306008da6159179c5d5ee1384803029f
oleibman <10341515+oleibman@users.noreply.github.com>
2025-08-25 19:32:10 -0700

    String Increment Deprecated in Php8.5 - release390

--- src/PhpSpreadsheet/Writer/Html.php
+++ src/PhpSpreadsheet/Writer/Html.php
@@ -514,7 +514,7 @@ class Html extends BaseWriter
                             $rowData[$column] = ($sheet->getCellCollection()->has($cellAddress)) ? $cellAddress : '';
                         }
                         ++$column;
-                        ++$colStr;
+                        StringHelper::stringIncrement($colStr);
                     }
                     $html .= $this->generateRow($sheet, $rowData, $row - 1, $cellType);
                 }
@@ -880,7 +880,7 @@ class Html extends BaseWriter
             if ($this->shouldGenerateColumn($sheet, $colStr)) {
                 $css['table.sheet' . $sheetIndex . ' col.col' . $column]['width'] = self::DEFAULT_CELL_WIDTH_POINTS . 'pt';
             }
-            ++$colStr;
+            StringHelper::stringIncrement($colStr);
         }
 
         // col elements, loop through columnDimensions and set width
