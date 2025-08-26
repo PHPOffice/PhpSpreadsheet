6c59ca8b497e8315bab1d74189d050ed8e59f652
oleibman <10341515+oleibman@users.noreply.github.com>
2025-08-25 19:39:28 -0700

    String Increment Deprecated in Php8.5 - release390 (#4602)

--- src/PhpSpreadsheet/Shared/StringHelper.php
+++ src/PhpSpreadsheet/Shared/StringHelper.php
@@ -643,4 +643,19 @@ class StringHelper
     {
         return strlen("$string");
     }
+
+    /**
+     * Php introduced str_increment with Php8.3,
+     * but didn't issue deprecation notices till 8.5.
+     *
+     * @codeCoverageIgnore
+     */
+    public static function stringIncrement(string &$str): void
+    {
+        if (function_exists('str_increment')) {
+            $str = str_increment($str);
+        } else {
+            ++$str;
+        }
+    }
 }
