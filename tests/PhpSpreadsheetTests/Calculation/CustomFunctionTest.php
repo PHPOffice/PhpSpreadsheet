f375d9a69b6ed3d7e9a2ecae0561034ce58ab698
oleibman <10341515+oleibman@users.noreply.github.com>
2025-09-01 00:28:46 -0700

    Minor Improvements to Calculation Coverage

--- tests/PhpSpreadsheetTests/Calculation/CustomFunctionTest.php
+++ tests/PhpSpreadsheetTests/Calculation/CustomFunctionTest.php
@@ -30,6 +30,15 @@ class CustomFunctionTest extends TestCase
         self::assertTrue(Calculation::removeFunction($key));
         self::assertSame('#NAME?', $calculation->calculateFormula('=FOURTHPOWER(3)'));
         self::assertFalse(Calculation::removeFunction('WHATEVER'));
+        $key = 'NATIVECOS';
+        $value = [
+            'category' => 'custom',
+            'functionCall' => 'cos',
+            'argumentCount' => '1',
+        ];
+        self::assertTrue(Calculation::addFunction($key, $value));
+        self::assertSame(1.0, $calculation->calculateFormula('=NATIVECOS(0)'));
+        self::assertTrue(Calculation::removeFunction($key));
     }
 
     public static function testReplaceDummyFunction(): void
