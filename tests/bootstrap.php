39a7b90b6fc13fe465c7389911e82c17da8da7f4
oleibman <10341515+oleibman@users.noreply.github.com>
2025-09-05 23:49:41 -0700

    WIP More Php8.5 Testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,9 +6,6 @@ setlocale(LC_ALL, 'en_US.utf8');
 
 function phpunit10ErrorHandler(int $errno, string $errstr, string $filename, int $lineno): bool
 {
-    if (strIncrement85(PHP_VERSION_ID, $errno, $errstr, $filename)) {
-        return true; // message suppressed - stop error handling
-    }
     $x = error_reporting() & $errno;
     if (
         in_array(
@@ -32,18 +29,6 @@ function phpunit10ErrorHandler(int $errno, string $errstr, string $filename, int
     }
 
     return false; // continue error handling
-}
-
-function strIncrement85(int $version, int $errno, string $errstr, string $filename): bool
-{
-    if ($version < 80500 || $errno !== E_DEPRECATED) {
-        return false;
-    }
-    if (preg_match('/canonical/', $errstr) === 1 && preg_match('/mitoteam/', $filename) === 1) {
-        return true;
-    }
-
-    return false;
 }
 
 if (!method_exists(PHPUnit\Framework\TestCase::class, 'setOutputCallback')) {
