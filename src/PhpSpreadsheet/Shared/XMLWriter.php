e49d7a9d94bfa59bf4763d28383a4e06addfe27a
oleibman <10341515+oleibman@users.noreply.github.com>
2025-09-10 10:16:28 -0700

    Small Improvement

--- src/PhpSpreadsheet/Shared/XMLWriter.php
+++ src/PhpSpreadsheet/Shared/XMLWriter.php
@@ -39,6 +39,9 @@ class XMLWriter extends \XMLWriter
             if (empty($this->tempFileName) || $this->openUri($this->tempFileName) === false) {
                 // Fallback to memory...
                 $this->openMemory();
+                if ($this->tempFileName != '') {
+                    @unlink($this->tempFileName);
+                }
                 $this->tempFileName = '';
             }
         }
