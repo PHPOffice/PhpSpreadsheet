a1ffd3473b256461ab60625c65a077b90495720d
oleibman <10341515+oleibman@users.noreply.github.com>
2025-09-29 22:25:20 -0700

    Offer Tcpdf Interface Which Throws Exception Rather than Die
    By default, TCPDF will die sometimes rather than throwing exception. And this is controlled by a defined constant in the global namespace, not by an instance property. Ugh! Using this class instead of the class which it extends will probably be suitable for most users. But not for those who have customized their config file. Which is why this isn't the default, so that there is no breaking change for those users. Note that if both TCPDF and TcpdfNoDie are used in the same process, the first one used "wins" the battle of the defines.

---
+++ tests/PhpSpreadsheetTests/Writer/Tcpdf/NoDieTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PhpOffice\PhpSpreadsheetTests\Writer\Tcpdf;
+
+use Exception;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Pdf\TcpdfNoDie;
+use PHPUnit\Framework\Attributes;
+
+// Separate processes because of global defined names
+#[Attributes\RunTestsInSeparateProcesses]
+class NoDieTest extends \PHPUnit\Framework\TestCase
+{
+    private Spreadsheet $spreadsheet;
+
+    protected function setUp(): void
+    {
+        $this->spreadsheet = new Spreadsheet();
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->spreadsheet);
+    }
+
+    public function testExceptionRatherThanDie(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Could not include font definition file');
+        $sheet = $this->spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'cell');
+        $writer = new TcpdfNoDie($this->spreadsheet);
+        $writer->setFont('xyz');
+        $writer->save('php://memory');
+    }
+}
