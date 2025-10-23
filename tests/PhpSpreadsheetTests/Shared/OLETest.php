d5b9ba68e2d4c62f967160af11052e390ebbc6b0
oleibman <10341515+oleibman@users.noreply.github.com>
2025-10-23 12:18:59 -0700

    New Tests

--- tests/PhpSpreadsheetTests/Shared/OLETest.php
+++ tests/PhpSpreadsheetTests/Shared/OLETest.php
@@ -75,4 +75,40 @@ class OLETest extends TestCase
             }
         }
     }
+
+    public function testOleFunctions(): void
+    {
+        $ole = new OLE();
+        $infile = 'tests/data/Reader/XLS/pr.4687.excel.xls';
+        $ole->read($infile);
+        self::assertSame(4, $ole->ppsTotal());
+        self::assertFalse($ole->isFile(0), 'root entry');
+        self::assertTrue($ole->isFile(1), 'workbook');
+        self::assertTrue($ole->isFile(2), 'summary information');
+        self::assertTrue($ole->isFile(3), 'document summary information');
+        self::assertFalse($ole->isFile(4), 'no such index');
+        self::assertTrue($ole->isRoot(0), 'root entry');
+        self::assertFalse($ole->isRoot(1), 'workbook');
+        self::assertFalse($ole->isRoot(2), 'summary information');
+        self::assertFalse($ole->isRoot(3), 'document summary information');
+        self::assertFalse($ole->isRoot(4), 'no such index');
+        self::assertSame(0, $ole->getDataLength(0), 'root entry');
+        self::assertSame(15712, $ole->getDataLength(1), 'workbook');
+        self::assertSame(4096, $ole->getDataLength(2), 'summary information');
+        self::assertSame(4096, $ole->getDataLength(3), 'document summary information');
+        self::assertSame(0, $ole->getDataLength(4), 'no such index');
+        self::assertSame('', $ole->getData(2, -1, 4), 'negative position');
+        self::assertSame('', $ole->getData(2, 5000, 4), 'position > length');
+        self::assertSame('feff0000', bin2hex($ole->getData(2, 0, 4)));
+        self::assertSame('', $ole->getData(4, 0, 4), 'no such index');
+    }
+
+    public function testBadEndian(): void
+    {
+        $this->expectException(ReaderException::class);
+        $this->expectExceptionMessage('Only Little-Endian encoding is supported');
+        $ole = new OLE();
+        $infile = 'tests/data/Reader/XLS/pr.4687.excel.badendian.xls';
+        $ole->read($infile);
+    }
 }
