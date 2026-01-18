ba3b2938cb2e2b0b1abd7a7fd033aab721ee754a
oleibman <10341515+oleibman@users.noreply.github.com>
2026-01-17 21:06:25 -0800

    Coverage Tweak

--- tests/PhpSpreadsheetTests/Reader/Html/StyleCheckBoxTest.php
+++ tests/PhpSpreadsheetTests/Reader/Html/StyleCheckBoxTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PhpOffice\PhpSpreadsheetTests\Reader\Html;
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Html as HtmlWriter;
 use PhpOffice\PhpSpreadsheetTests\Functional\AbstractFunctional;
 
 class StyleCheckBoxTest extends AbstractFunctional
@@ -20,6 +21,35 @@ class StyleCheckBoxTest extends AbstractFunctional
         $sheetOld->getStyle('A2')->setCheckBox(true);
 
         $spreadsheet = $this->writeAndReload($spreadsheetOld, 'Html');
+        $spreadsheetOld->disconnectWorksheets();
+
+        $sheet = $spreadsheet->getActiveSheet();
+        self::assertTrue($sheet->getCell('A1')->getValue());
+        self::assertFalse($sheet->getCell('A2')->getValue());
+        self::assertFalse($sheet->getCell('A3')->getValue());
+        self::assertTrue($sheet->getStyle('A1')->getCheckBox());
+        self::assertTrue($sheet->getStyle('A2')->getCheckBox());
+        self::assertFalse($sheet->getStyle('A3')->getCheckBox());
+
+        $spreadsheet->disconnectWorksheets();
+    }
+
+    private function writeNoPreCalc(HtmlWriter $writer): void
+    {
+        $writer->setPreCalculateFormulas(false);
+    }
+
+    public function testStyleCheckBoxNoPreCalculate(): void
+    {
+        $spreadsheetOld = new Spreadsheet();
+        $sheetOld = $spreadsheetOld->getActiveSheet();
+        $sheetOld->setCellValue('A1', true);
+        $sheetOld->setCellValue('A2', false);
+        $sheetOld->setCellValue('A3', false); // no checkbox
+        $sheetOld->getStyle('A1')->setCheckBox(true);
+        $sheetOld->getStyle('A2')->setCheckBox(true);
+
+        $spreadsheet = $this->writeAndReload($spreadsheetOld, 'Html', null, $this->writeNoPreCalc(...));
         $spreadsheetOld->disconnectWorksheets();
 
         $sheet = $spreadsheet->getActiveSheet();
