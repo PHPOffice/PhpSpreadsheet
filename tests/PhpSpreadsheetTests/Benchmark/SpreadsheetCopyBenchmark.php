afac1badab53d3a476cc6bd3afc0468b18e974e8
kemo <kemal.delalic@gmail.com>
2026-03-10 23:59:21 +0100

    Fix phpstan and code style errors

--- tests/PhpSpreadsheetTests/Benchmark/SpreadsheetCopyBenchmark.php
+++ tests/PhpSpreadsheetTests/Benchmark/SpreadsheetCopyBenchmark.php
@@ -156,6 +156,7 @@ class SpreadsheetCopyBenchmark extends TestCase
         $serializeEnd = hrtime(true);
         $serializeMemAfter = memory_get_usage(true);
         foreach ($serialized as $s) {
+            /** @var \PhpOffice\PhpSpreadsheet\Spreadsheet $s */
             $s->disconnectWorksheets();
         }
         unset($serialized);
