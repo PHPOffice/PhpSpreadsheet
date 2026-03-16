8f639deb0c210aa01c7ab359421ad24f045077ed
kemo <kemal.delalic@gmail.com>
2026-03-16 15:01:04 +0100

    Move benchmarks to tests/Benchmark to avoid running with unit tests

--- tests/Benchmark/XlsxZipReadCacheBenchmark.php
+++ tests/Benchmark/XlsxZipReadCacheBenchmark.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace PhpOffice\PhpSpreadsheetTests\Benchmark;
+namespace PhpOffice\PhpSpreadsheetBenchmarks;
 
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx as XlsxReader;
