6798178ce44f4a7496ea1a9cfd5d6d1229dfc7c0
kemo <kemal.delalic@gmail.com>
2026-03-16 15:01:04 +0100

    Move benchmarks to tests/Benchmark to avoid running with unit tests

--- tests/Benchmark/XlsxStreamingReaderBenchmark.php
+++ tests/Benchmark/XlsxStreamingReaderBenchmark.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace PhpOffice\PhpSpreadsheetTests\Benchmark;
+namespace PhpOffice\PhpSpreadsheetBenchmarks;
 
 use PhpOffice\PhpSpreadsheet\Cell\DataType;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
