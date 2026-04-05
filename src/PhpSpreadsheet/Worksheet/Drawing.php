87a0eb5442987300d0a84fb9f0ce197efcf25bb9
oleibman <10341515+oleibman@users.noreply.github.com>
2026-04-04 23:29:26 -0700

    Option to Whitelist External Images r129 (#4854)

--- src/PhpSpreadsheet/Worksheet/Drawing.php
+++ src/PhpSpreadsheet/Worksheet/Drawing.php
@@ -102,10 +102,11 @@ class Drawing extends BaseDrawing
      * @param bool $verifyFile Verify file
      * @param ZipArchive $zip Zip archive instance
      * @param bool $allowExternal
+     * @param null|callable(string):bool $isWhitelisted
      *
      * @return $this
      */
-    public function setPath($path, $verifyFile = true, $zip = null, $allowExternal = true)
+    public function setPath($path, $verifyFile = true, $zip = null, $allowExternal = true, ?callable $isWhitelisted = null)
     {
         $this->isUrl = false;
         if (preg_match('~^data:image/[a-z]+;base64,~', $path) === 1) {
@@ -115,12 +116,24 @@ class Drawing extends BaseDrawing
         }
 
         $this->path = '';
+        if ($zip instanceof ZipArchive) {
+            $zipPath = explode('#', $path)[1];
+            $locate = @$zip->locateName($zipPath);
+            if ($locate !== false) {
+                if ($this->isImage($path)) {
+                    $this->path = $path;
+                    $this->setSizesAndType($path);
+                }
+            }
         // Check if a URL has been passed. https://stackoverflow.com/a/2058596/1252979
-        if (filter_var($path, FILTER_VALIDATE_URL) || (preg_match('/^([\w\s\x00-\x1f]+):/u', $path) && !preg_match('/^([\w]+):/u', $path))) {
+        } elseif (filter_var($path, FILTER_VALIDATE_URL) || (preg_match('/^([\w\s\x00-\x1f]+):/u', $path) && !preg_match('/^([\w]+):/u', $path))) {
             if (!preg_match('/^(http|https|file|ftp|s3):/', $path)) {
                 throw new PhpSpreadsheetException('Invalid protocol for linked drawing');
             }
             if (!$allowExternal) {
+                return $this;
+            }
+            if ($isWhitelisted !== null && !$isWhitelisted($path)) {
                 return $this;
             }
             // Implicit that it is a URL, rather store info than running check above on value in other places.
@@ -155,15 +168,6 @@ class Drawing extends BaseDrawing
                         }
                         unlink($filePath);
                     }
-                }
-            }
-        } elseif ($zip instanceof ZipArchive) {
-            $zipPath = explode('#', $path)[1];
-            $locate = @$zip->locateName($zipPath);
-            if ($locate !== false) {
-                if ($this->isImage($path)) {
-                    $this->path = $path;
-                    $this->setSizesAndType($path);
                 }
             }
         } else {
--- tests/PhpSpreadsheetTests/Reader/Html/HtmlHelper.php
+++ tests/PhpSpreadsheetTests/Reader/Html/HtmlHelper.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PhpOffice\PhpSpreadsheetTests\Reader\Html;
 
 use PhpOffice\PhpSpreadsheet\Reader\Html;
@@ -26,6 +28,17 @@ class HtmlHelper
         if ($unlink) {
             unlink($filename);
         }
+
+        return $spreadsheet;
+    }
+
+    public static function loadHtmlStringIntoSpreadsheet(string $content, ?bool $allowExternalImages = null): Spreadsheet
+    {
+        $html = new Html();
+        if ($allowExternalImages !== null) {
+            $html->setAllowExternalImages($allowExternalImages);
+        }
+        $spreadsheet = $html->loadFromString($content);
 
         return $spreadsheet;
     }
