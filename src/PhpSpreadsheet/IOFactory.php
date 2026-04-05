93c94eb31d59b8248b1d510e1fb5f2f46bc77dce
oleibman <10341515+oleibman@users.noreply.github.com>
2026-04-04 21:04:58 -0700

    Merge commit from fork
    * Test Protocol Not Supplied Before Using is_file
    
    * added missing assertFile and renamed prohibitPhar
    
    * More Tests
    
    ---------
    
    Co-authored-by: calligraf0 <115566010+calligraf0@users.noreply.github.com>

--- src/PhpSpreadsheet/IOFactory.php
+++ src/PhpSpreadsheet/IOFactory.php
@@ -285,4 +285,16 @@ abstract class IOFactory
 
         self::$readers[$readerType] = $readerClass;
     }
+
+    /**
+     * @return array<string, class-string<IReader>>
+     *
+     * @internal
+     *
+     * @codeCoverageIgnore
+     */
+    public static function getReaders(): array
+    {
+        return self::$readers;
+    }
 }
